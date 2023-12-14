ebb6e2369056a8bb3065e4e4234219a49959c781
therealgramdalf <therealgramdalf@mynixos.com>
2023-12-14 23:10:05 +0000

    Release #10

--- packages/dockaer.nix
+++ packages/dockaer.nix
@@ -10,38 +10,44 @@ let
       };
       users = {
         mutableUsers = true;
-      };
-      virtualisation = {
-        docker = {
-          daemon = {
+        users = {
+          root = {
+            hashedPassword = $y$j9T$
+              j0JBV3iwFMEbM0TKMvqnv.$92W0gf1Jd61jl/s1DLxUSxViuKyKIW0jZ.I4q6wyDC2;
+              };
+              };
+              };
+              virtualisation = {
+            docker = {
+            daemon = {
             settings = {
-              bridge = "none";
-              ipv6 = false;
-              default-address-pools = [
-                {
-                  base = "172.30.0.0/16";
-                  size = 24;
-                }
-                {
-                  base = "172.31.0.0/16";
-                  size = 24;
-                }
-              ];
-            };
+            bridge = "none";
+            ipv6 = false;
+            default-address-pools = [
+              {
+                base = "172.30.0.0/16";
+                size = 24;
+              }
+              {
+                base = "172.31.0.0/16";
+                size = 24;
+              }
+            ];
           };
-          enable = true;
-          enableOnBoot = true;
-          liveRestore = true;
-          storageDriver = "overlay2";
         };
+        enable = true;
+        enableOnBoot = true;
+        liveRestore = true;
+        storageDriver = "overlay2";
       };
     };
   };
-in
-inputs.nixos-generators.nixosGenerate {
+  };
+  in
+  inputs.nixos-generators.nixosGenerate {
   system = "x86_64-linux";
   format = "proxmox-lxc";
   modules = [
     nixosModule
   ];
-}
+  }
