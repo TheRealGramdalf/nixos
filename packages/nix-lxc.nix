3592de6b1d8deb109dba2114ea34ba198ed56f00
therealgramdalf <therealgramdalf@mynixos.com>
2023-12-18 20:47:09 +0000

    Release #25

--- packages/nix-lxc.nix
+++ packages/nix-lxc.nix
@@ -3,7 +3,6 @@ let
   nixosModule = { config, lib, pkgs, ... }: {
     imports = [
       inputs.self.nixosModules.docker-ve
-      inputs.self.nixosModules.gettyFix
     ];
     config = {
       services = {
