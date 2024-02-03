341ef102e8bf40bb78eb8bd17a2bd96d34e09d1c
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-02-03 13:44:19 +0100

    Local changes autocommit

--- home.nix
+++ home.nix
@@ -118,11 +118,28 @@
     ];
   };
   
-  /*wayland.windowManager.hyprland.settings = {
+  wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
     
     bind = [
-      "$mod, B, e"*/
+      "$mod, B, exec, librewolf"
+    ]
+    ++ (
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
+  };
 
   programs = {
     home-manager.enable = true;
