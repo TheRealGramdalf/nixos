47eec7f725207dfdc0b8b7e98e1104cca1d111cc
mars-monkey <91227993+mars-monkey@users.noreply.github.com>
2024-02-11 07:01:35 +0100

    Local changes autocommit

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    /*snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };*/
-
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,19 +12,7 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs =  { self, nixpkgs, home-manager, hyprland}@inputs: /*inputs:
-    inputs.snowfall-lib.mkFlake {
-      inherit inputs;
-      src = ./.;
-      
-      snowfall = {
-        
-
-
-*/
-
-
- {
+  outputs =  { self, nixpkgs, home-manager, hyprland }@inputs: {
     nixosConfigurations = {
       "mars-monkey-laptop" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -53,15 +36,13 @@
   };
   
   nixConfig = {
-    substituters = [
+    extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
       "https://hyprland.cachix.org"
     ];
     
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
