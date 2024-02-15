177dd0275985276ac8303450ee041231bd99ecd8
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-15 11:41:12 -0800

    Add ripjaw to `flake.nix`

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "mars-monkey's experimental config";
+  description = "TheRealGramdalf's experimental config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -12,20 +12,25 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland }: {
+  outputs = { self, nixpkgs, home-manager, hyprland, pkgs, lib, config, ... }: {
+    
     nixosConfigurations = {
-      "mars-monkey-laptop" = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit hyprland; };
-
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
+      "ripjaw" = lib.nixosSystem {
+        #specialArgs = { inherit hyprland; };
+        imports = [
+          ./config/hosts/ripjaw/main.nix
         ];
+        /*modules = [
+          ./config/hosts/ripjaw/main.nix
+          home-manager.nixosModules.home-manager
+        ];*/
       };
     };
   };
 
+  # Global nix.settings, these are presented to the user as an optional choice on rebuild
   nixConfig = {
+    # See https://nixos.org/manual/nix/stable/command-ref/conf-file.html?highlight=you%20can%20prefix%20the%20name%20of%20the%20setting%20by%20extra-%20to%20append%20to%20the%20previous%20value#file-format
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
