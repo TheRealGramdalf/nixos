439c7853d067d2a54e7eb0d6b48616f761c716a1
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-16 13:41:54 -0800

    Fix zfs-hybrid

--- config/disko/zfs-hybrid.nix
+++ config/disko/zfs-hybrid.nix
@@ -9,22 +9,25 @@
         partitions = {
           # This allows a GPT partition table to be used with legacy BIOS systems. See https://www.gnu.org/software/grub/manual/grub/html_node/BIOS-installation.html
           "mbr" = {
+            label = "mbr";
             size = "1M";
             type = "EF02";
           };
+          "zroot" = {
+            label = "zroot";
+            end = "-512M"; # Negative end means "Leave this much empty space at the end of the device"
+            content = {
+              type = "zfs";
+              pool = "zroot";
+          };};
           "ZSYS" = {
-            end = "-512M"; # Negative size to place it at the end of the partition
-            type = "EF00"; # ^^ Mostly desirable with resizeable partitions like BTRFS/EXT4, used here for compatibility
+            label = "ZSYS";
+            size = "100%";
+            type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-          };};
-          "zroot" = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "zroot";
           };};
         };
       };
@@ -53,6 +56,7 @@
           options = {
             atime = "off";
             canmount = "noauto";
+            mountpoint = "legacy"; # See https://github.com/nix-community/disko/issues/298#issuecomment-1949322912
         };};
         "safe" = {
           type = "zfs_fs";
@@ -65,12 +69,14 @@
           mountpoint = "/persist";
           options = {
             canmount = "noauto";
+            mountpoint = "legacy"; # See https://github.com/nix-community/disko/issues/298#issuecomment-1949322912
         };};
         "safe/home" = {
           type = "zfs_fs";
           mountpoint = "/home";
           options = {
             canmount = "noauto";
+            mountpoint = "legacy"; # See https://github.com/nix-community/disko/issues/298#issuecomment-1949322912
         };};
         "system-state" = {
           type = "zfs_fs";
@@ -89,14 +95,13 @@
 # https://github.com/nix-community/disko/issues/298
 # Configure disk format in line with `zpool`, rather than separately
 
-#2024-02-06.13:05:06 zpool create -o ashift=12 -O compression=zstd -O xattr=sa -O acltype=posix -O mountpoint=none -R /mnt aerwiar-zpool /dev/disk/by-partlabel/aerwiar-zpool
-#2024-02-06.13:05:06 zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/ephemeral
-#2024-02-06.13:05:06 zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/safe
-#2024-02-06.13:05:06 zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/system-state
-#2024-02-06.13:05:06 zfs create -o readonly=on aerwiar-zpool/system-state/boot
-#2024-02-06.13:05:06 zfs create -o readonly=on aerwiar-zpool/safe/home
-#2024-02-06.13:05:06 zfs create aerwiar-zpool/safe/home/gramdalf
-#2024-02-06.13:05:06 zfs create -o atime=off aerwiar-zpool/ephemeral/nix
-#2024-02-06.13:05:06 zfs snap -r aerwiar-zpool@blank
-#2024-02-06.16:22:01 zpool import aerwiar-zpool -d /dev/disk/by-partlabel
-#2024-02-06.16:29:37 zfs create -o canmount=noauto aerwiar-zpool/safe/persist
+#zpool create -o ashift=12 -O compression=zstd -O xattr=sa -O acltype=posix -O mountpoint=none -R /mnt aerwiar-zpool /dev/disk/by-partlabel/aerwiar-zpool
+#zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/ephemeral
+#zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/safe
+#zfs create -o canmount=noauto -o mountpoint=legacy aerwiar-zpool/system-state
+#zfs create -o readonly=on aerwiar-zpool/system-state/boot
+#zfs create -o readonly=on aerwiar-zpool/safe/home
+#zfs create aerwiar-zpool/safe/home/gramdalf
+#zfs create -o atime=off aerwiar-zpool/ephemeral/nix
+#zfs snap -r aerwiar-zpool@blank
+#zfs create -o canmount=noauto aerwiar-zpool/safe/persist
