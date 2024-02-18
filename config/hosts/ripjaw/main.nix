c87709f7ebc2edc6832f05b298ae16e7329bdc1e
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-18 14:08:36 -0800

    Disable devnodes override

--- config/hosts/ripjaw/main.nix
+++ config/hosts/ripjaw/main.nix
@@ -11,9 +11,9 @@
     #./home.nix
   ];
   # Override zfs devnodes for running in a VM
-  boot.zfs = lib.mkForce {
-    devNodes = "/dev/disk/by-path";
-  };
+  #boot.zfs = lib.mkForce {
+  #  devNodes = "/dev/disk/by-path";
+  #};
   system.stateVersion = "24.05";
   networking = {
     hostName = "ripjaw";
