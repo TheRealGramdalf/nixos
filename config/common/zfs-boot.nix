130ee7ba98c8e0145dc4b2120769da4b17484230
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-18 18:54:09 -0800

    Fix zfs boot maybe?

--- config/common/zfs-boot.nix
+++ config/common/zfs-boot.nix
@@ -1,9 +1,9 @@
 { config, ... }: {
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  boot.zfs = {
+  #boot.zfs = {
     #enabled = true; # This is enabled automatically
-    devNodes = /dev/disk/by-partlabel;
-  };
+    #devNodes = /dev/disk/by-partlabel; # Might have issues with legacy BIOS, since it can't read GPT labels?
+  #};
   # Since swapfile isn't available, enable zramswap
   zramSwap.enable = true;
 }
