81c0e39929048b5631e1392e69577ee55e9ea464
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-21 13:57:50 -0800

    Expand kanidm unix settings

--- config/common/posix-client.nix
+++ config/common/posix-client.nix
@@ -2,5 +2,14 @@
   services.kanidm = {
     enablePam = true;
     clientSettings.uri = "https://auth.aer.dedyn.io";
+    unixSettings = {
+      home_prefix = "/home/";
+      home_attr = "uuid";
+      home_alias = "spn";
+      use_etc_skel = false;
+      uid_attr_map = "spn";
+      gid_attr_map = "spn";
+      selinux = true;
+    };
   };
 }
