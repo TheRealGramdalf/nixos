ef1fca2223fc2789117e20cdf6c09cf26dab1c41
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-21 13:59:10 -0800

    Add docker-ve

---
+++ config/hosts/docker-ve/main.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, modulesPath, ... }: {
+  imports = [
+    # Commonly used config
+    ../../common/posix-client.nix
+    ../../common/avahi-client.nix
+
+    # Host-specific config
+    ./configuration.nix
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
+  ];
+  system.stateVersion = "24.05";
+  networking.hostName = "docker-ve";
+}
