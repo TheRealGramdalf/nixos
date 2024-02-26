78ab19f106bb68b47a4554b398bf775fd8c6d366
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-25 18:31:54 -0800

    Add pipewire commonconf

---
+++ config/common/pipewire.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, ... }: {
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  }
+}
