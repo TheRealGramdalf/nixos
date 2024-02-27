4cac5b37b6965c61a839f80be3554d9bff7d865c
TheRealGramdalf <gramdalftech@gmail.com>
2024-02-27 13:27:24 -0800

    Add ARM service user

--- config/hosts/ripjaw/arm.nix
+++ config/hosts/ripjaw/arm.nix
@@ -1,30 +1,34 @@
 { config, lib, pkgs, ... }: {
+  users.users."docker-ripjaw" = {
+    description = "A.R.M. service account";
+    home = "/persist/docker-ripjaw";
+    uid = 911; # Lord of The Rings: The Twin Towers
+    gid = 911;
+  };
   virtualisation.oci-containers.backend = "docker";
-  virtualisation.oci-containers.containers = {
-    "ripjaw" = {
-      autoStart = true;
-      image = "automaticrippingmachine/automatic-ripping-machine:latest";
-      ports = [ "8080:8080" ];
-      volumes = [
-        "/persist/ripjaw/home:/home/arm"
-        "/persist/ripjaw/music:/home/arm/Music"
-        "/persist/ripjaw/logs:/home/arm/logs"
-        "/persist/ripjaw/media:/home/arm/media"
-        "/persist/ripjaw/config:/etc/arm/config"
-      ];
-      #environment = {
-      #  ARM_UID = "";
-      #  ARM_GID = "";
-      #};
-      extraOptions = [
-        # Needed for ARM to work correctly - by default `CAP_SYS_ADMIN` is dropped
-        # which blocks `mount()` calls within the container
-        # This is needed in order to `mount /dev/sr0 /mnt/dev/sr0` for ripping, which may be avoidable by
-        # handling mounts outside of the container, and having `/mnt/dev` bind mounted into the container.
-        "--privileged"
-        "--device=/dev/sr0:/dev/sr0"
-        "--device=/dev/sr1:/dev/sr1"
-      ];
+  virtualisation.oci-containers.containers."ripjaw" = {
+    autoStart = true;
+    image = "automaticrippingmachine/automatic-ripping-machine:latest";
+    ports = [ "8080:8080" ];
+    volumes = [
+      "/persist/docker-ripjaw/home:/home/arm"
+      "/persist/docker-ripjaw/music:/home/arm/Music"
+      "/persist/docker-ripjaw/logs:/home/arm/logs"
+      "/persist/docker-ripjaw/media:/home/arm/media"
+      "/persist/docker-ripjaw/config:/etc/arm/config"
+    ];
+    environment = {
+      ARM_UID = "911";
+      ARM_GID = "911";
     };
+    extraOptions = [
+      # Needed for ARM to work correctly - by default `CAP_SYS_ADMIN` is dropped
+      # which blocks `mount()` calls within the container
+      # This is needed in order to `mount /dev/sr0 /mnt/dev/sr0` for ripping, which may be avoidable by
+      # handling mounts outside of the container, and having `/mnt/dev` bind mounted into the container.
+      "--privileged"
+      "--device=/dev/sr0:/dev/sr0"
+      "--device=/dev/sr1:/dev/sr1"
+    ];
   };
 }
