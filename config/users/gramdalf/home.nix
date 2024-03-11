0b6c0b8298efe298fa77d727746918c2f74e950c
TheRealGramdalf <gramdalftech@gmail.com>
2024-03-11 15:39:25 -0700

    Split ddterm

--- config/users/gramdalf/home.nix
+++ config/users/gramdalf/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./zsh.nix
+    ./ddterm.nix
   ];
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
@@ -186,8 +187,6 @@
       gnomeExtensions.removable-drive-menu
       gnomeExtensions.espresso
       gnomeExtensions.forge
-      gnomeExtensions.ddterm
-      gjs
       gnomeExtensions.blur-my-shell
       gnomeExtensions.just-perfection
       # gnomeExtensions.noannoyance-fork  # To add
@@ -247,27 +246,6 @@
   };
   
   dconf.settings = {
-    "com/github/amezin/ddterm" = {
-      # vi/vim requires escape to function
-      hide-window-on-esc = false;
-      scrollback-unlimited = true;
-      show-scrollbar = false;
-      notebook-border = false;
-      ddterm-toggle-hotkey = [ "F1" ];
-      panel-icon-type = "none";
-      # Color profile. To migrate to a gnome-terminal profile, see: 
-      #  https://mynixos.com/home-manager/options/programs.gnome-terminal.profile.%3Cname%3E
-      # Use (FG/BG) colors specified below, not system defaults
-      use-theme-colors = false;
-      foreground-color = "rgb(230,230,230)"; # Text color
-      background-color = "rgb(31,34,41)"; # Backdrop color
-      background-opacity = 0.9;
-      palette = [
-        "rgb(23,20,33)" "rgb(192,28,40)" "rgb(94,189,171)" "rgb(162,115,76)" "rgb(54,123,240)" "rgb(163,71,186)" "rgb(42,161,179)" "rgb(208,207,204)"
-        "rgb(94,92,100)" "rgb(246,97,81)" "rgb(51,218,122)" "rgb(233,173,12)" "rgb(42,123,222)" "rgb(192,97,203)" "rgb(51,199,222)" "rgb(255,255,255)"
-      ];
-      # Default palette: ['rgb(23,20,33)', 'rgb(192,28,40)', 'rgb(38,162,105)', 'rgb(162,115,76)', 'rgb(18,72,139)', 'rgb(163,71,186)', 'rgb(42,161,179)', 'rgb(208,207,204)', 'rgb(94,92,100)', 'rgb(246,97,81)', 'rgb(51,218,122)', 'rgb(233,173,12)', 'rgb(42,123,222)', 'rgb(192,97,203)', 'rgb(51,199,222)', 'rgb(255,255,255)']
-    };
     "org/gnome/desktop/interface" = {
       cursor-theme = "SpaceKCursors";
       color-scheme = "prefer-dark";
