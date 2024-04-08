71a801e670efc4eb5922da1a7a5fbfa807002a55
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-08 10:03:13 -0700

    Remove bloat

--- config/hosts/atreus/hypr.nix
+++ config/hosts/atreus/hypr.nix
@@ -3,12 +3,22 @@
   # Add a terminal 
   # So the default super + m keybind works
   environment.systemPackages = [ pkgs.kitty ];
+  
   # Enable networkmanager
   networking.networkmanager.enable = true;
+
+
   ## SDDM ##
   # Todo: Override this so that x11 isn't needed for hyprland
   # Most of the bloat can probably be fixed with excludePackages
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    excludePackages = with pkgs; [
+      xterm
+      # ssh-askpass # Not sure which package it is
+    ];
+  };
+
   services.xserver.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
@@ -21,5 +31,4 @@
       #libsForQt5.qt5.qtsvg # Not needed?
     ];
   };
-
 }
