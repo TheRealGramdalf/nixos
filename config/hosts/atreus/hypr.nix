29485bb9ad1a17cff56bb641d5590defd471b5a6
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-08 11:25:56 -0700

    Clean up catpuccin

--- config/hosts/atreus/hypr.nix
+++ config/hosts/atreus/hypr.nix
@@ -22,12 +22,9 @@
   services.xserver.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "${pkgs.catppuccin-sddm-corners}/share/sddm/themes/catppuccin-sddm-corners"; # This method is probably preferred
-    #theme = "catppuccin-sddm-corners";
-    #theme = "${pkgs.catppuccin-sddm-corners}";
+    theme = "${pkgs.catppuccin-sddm-corners}/share/sddm/themes/catppuccin-sddm-corners"; # Not including `Main.qml`, since SDDM does this automagically
     extraPackages = [
       pkgs.libsForQt5.qt5.qtgraphicaleffects
-      #pkgs.catppuccin-sddm-corners # Might not work
       #libsForQt5.qt5.qtsvg # Not needed?
     ];
   };
