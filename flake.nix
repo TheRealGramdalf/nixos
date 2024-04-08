cac1b33db8bf83cb65a3534e109130e2987846b2
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-08 12:34:47 -0700

    Activate anyrun

--- flake.nix
+++ flake.nix
@@ -18,10 +18,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    #anyrun = {
-    #  url = "github:anyrun-org/anyrun";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    anyrun = {
+      url = "github:anyrun-org/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     #hyprland.url = "github:hyprwm/Hyprland";
   };
