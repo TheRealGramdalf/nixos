b2500b6f35f1946434ed31daf7b6af3c9ed5ac20
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 14:22:27 -0700

    You've been gnomed

--- config/users/gramdalf/waybar-interim.nix
+++ config/users/gramdalf/waybar-interim.nix
@@ -2,5 +2,14 @@
   programs.waybar = {
     enable = true;
     systemd.enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        "hyprland/workspaces" = {disable-scroll = false;};
+        modules-left = ["hyprland/workspaces"];
+        modules-center = ["clock"];
+        modules-right = [ "tray" "cpu" "backlight" "battery"];
+      };
+    };
   };
 }
