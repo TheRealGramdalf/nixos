029c91639f29bd669a83b26f495f072fc844623a
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 11:57:52 -0700

    AreWeWaylandYet?

--- config/hosts/aerwiar/hypr/hypr.nix
+++ config/hosts/aerwiar/hypr/hypr.nix
@@ -4,6 +4,15 @@
   # So the default super + m keybind works
   environment.systemPackages = [ pkgs.kitty ];
   
+  # Use wayland pls uwu
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    GDK_BACKEND = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+  };
+
   # Enable networkmanager
   networking.networkmanager.enable = true;
   imports = [
