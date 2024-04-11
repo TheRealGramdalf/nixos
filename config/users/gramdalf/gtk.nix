38b3e3e7597772a395033a779adfc64a5cf85e91
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 17:49:55 -0700

    Enable gtk4

--- config/users/gramdalf/gtk.nix
+++ config/users/gramdalf/gtk.nix
@@ -11,9 +11,9 @@
     };
   };
   # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
-  #xdg.configFile = {
-  #  "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-  #  "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-  #  "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-  #};
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 }
