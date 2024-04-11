d2c908c570b0886b19df67eac1b26787d76989cc
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 18:48:27 -0700

    Enable wayland wherever possible

--- config/hosts/atreus/hypr/hypr.nix
+++ config/hosts/atreus/hypr/hypr.nix
@@ -4,6 +4,16 @@
   # So the default super + m keybind works
   environment.systemPackages = [ pkgs.kitty ];
   
+  # Use wayland pls uwu
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    GDK_BACKEND = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
   # Enable networkmanager
   networking.networkmanager.enable = true;
   imports = [
