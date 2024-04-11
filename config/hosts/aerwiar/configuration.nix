822c3a4fc808de30fd76a4343d9028be6b771bda
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 18:36:33 -0700

    Remove temporary user, duplicate xserver

--- config/hosts/aerwiar/configuration.nix
+++ config/hosts/aerwiar/configuration.nix
@@ -21,14 +21,12 @@
     };
   };
   
-  nix = {
-    settings = {
+  nix.settings = {
       auto-optimise-store = true;
       experimental-features = [
         "nix-command"
         "flakes"
       ];
-    };
   };
   
   i18n.defaultLocale = "en_US.UTF-8";
@@ -37,12 +35,6 @@
     kanidm.enableClient = true;
     kanidm.clientSettings.uri = "https://auth.aer.dedyn.io";
     fwupd.enable = true;
-    xserver = {
-      enable = true;
-      xkb.layout = "us";
-      # libinput.enable = true;
-      excludePackages = [ pkgs.xterm ];
-    };
   };  
   
   # Docker
@@ -59,31 +51,6 @@
       extraGroups = [ "wheel" "networkmanager" "docker" "adbusers" "wireshark" ];
       hashedPasswordFile = "/persist/secrets/passwdfile.gramdalf";
       shell = pkgs.zsh;
-      
-      packages = with pkgs; [
-        home-manager
-        git
-        neovim
-        nerdfonts
-        wget
-        curl
-     ];
-    };
-    users."hyprgramdalf" = {
-      isNormalUser = true;
-      description = "Hyprland Gramdalf";
-      extraGroups = [ "wheel" "networkmanager" "docker" "adbusers" "wireshark" ];
-      hashedPasswordFile = "/persist/secrets/passwdfile.gramdalf";
-      shell = pkgs.zsh;
-      
-      packages = with pkgs; [
-        home-manager
-        git
-        neovim
-        nerdfonts
-        wget
-        curl
-     ];
     };
   };    
 }
