0e26074283d42ff1d9df544f83f7f32037f98cac
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-10 17:50:11 -0700

    Beginnings of ff-ultima

--- config/users/gramdalf/firefox.nix
+++ config/users/gramdalf/firefox.nix
@@ -1,16 +1,29 @@
-{ config, lib, pkgs, fetchFromGithub, ... }: {
+{ config, lib, pkgs, ... }: {
   programs.firefox = {
     enable = true;
     profiles."gramdalf" = {
        isDefault = true;
-    }
+       userChrome = builtins.readFile (pkgs.fetchFromGitHub {
+        owner = "soulhotel";
+        repo = "FF-ULTIMA";
+        rev = "1.6.8";
+        hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
+      } + "/userChrome.css");
+      userContent = builtins.readFile (pkgs.fetchFromGitHub {
+        owner = "soulhotel";
+        repo = "FF-ULTIMA";
+        rev = "1.6.8";
+        hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
+      } + "/userContent.css");
+    };
   };
 
+# https://stackoverflow.com/questions/53658303/fetchfromgithub-filter-down-and-use-as-environment-etc-file-source
 
-  home.file.".firefox/ff-ultima".source = fetchFromGithub {
+  home.file.".firefox/ff-ultima".source = pkgs.fetchFromGitHub {
     owner = "soulhotel";
-    repo = FF-ULTIMA;
+    repo = "FF-ULTIMA";
     rev = "1.6.8";
-    hash = "invalid-nonsense-hash";
-  }
+    hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
+  };
 }
