f5d3388dbb31c2a7bbdad382502bebe7006bd042
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-12 14:16:39 -0700

    Firefox un-ultima :(

--- config/users/gramdalf/firefox.nix
+++ config/users/gramdalf/firefox.nix
@@ -2,25 +2,17 @@
   programs.firefox = {
     enable = true;
     profiles."gramdalf" = {
-       isDefault = true;
-       userChrome = builtins.readFile (pkgs.fetchFromGitHub {
-        owner = "soulhotel";
-        repo = "FF-ULTIMA";
-        rev = "1.6.8";
-        hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
-      } + "/userChrome.css");
-      userContent = builtins.readFile (pkgs.fetchFromGitHub {
-        owner = "soulhotel";
-        repo = "FF-ULTIMA";
-        rev = "1.6.8";
-        hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
-      } + "/userContent.css");
+     isDefault = true;
+     settings = {
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+     };
     };
   };
 
 # https://stackoverflow.com/questions/53658303/fetchfromgithub-filter-down-and-use-as-environment-etc-file-source
 
-  home.file.".firefox/ff-ultima".source = pkgs.fetchFromGitHub {
+  home.file.".mozilla/firefox/gramdalf/chrome".source = pkgs.fetchFromGitHub {
     owner = "soulhotel";
     repo = "FF-ULTIMA";
     rev = "1.6.8";
