026f06eb0fb1125a0f1bac727fe515c964185cd2
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-15 21:27:28 -0700

    Your eyes will thank you

--- config/users/gramdalf/hypr/gtk.nix
+++ config/users/gramdalf/hypr/gtk.nix
@@ -4,6 +4,7 @@
   pkgs,
   ...
 }: {
+  # Use `nwg-look` to test out themes
   gtk = {
     enable = true;
     theme = {
@@ -20,5 +21,9 @@
     "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
     "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
     "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
+  # Enable dark mode (also enables catppuccin, since it is considered a 'dark' variant)
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
   };
 }
