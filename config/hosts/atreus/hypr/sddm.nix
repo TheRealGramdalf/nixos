e9c3a9013e72e2ecd786f2d6b161cc75314376e3
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-19 22:32:14 -0700

    Update hyprland config

--- config/hosts/atreus/hypr/sddm.nix
+++ config/hosts/atreus/hypr/sddm.nix
@@ -4,17 +4,7 @@
   pkgs,
   ...
 }: {
-  # Todo: Override this so that x11 isn't needed for hyprland
-  # Most of the bloat can probably be fixed with excludePackages
-  services.xserver = {
-    enable = true;
-    excludePackages = with pkgs; [
-      xterm
-      x11_ssh_askpass
-    ];
-  };
-
-  services.xserver.displayManager.sddm = {
+  services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     theme = "${pkgs.catppuccin-sddm-corners}/share/sddm/themes/catppuccin-sddm-corners"; # Not including `Main.qml`, since SDDM does this automagically
