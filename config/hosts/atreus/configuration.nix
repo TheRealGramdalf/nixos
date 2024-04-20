ddcda901a3c0b20542e887de1ac38bf582c9ee4e
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-19 22:52:08 -0700

    Add passwordfiles

--- config/hosts/atreus/configuration.nix
+++ config/hosts/atreus/configuration.nix
@@ -1,11 +1,14 @@
 {
+  users.users = {
+    "meebling".hashedPasswordFile = "/etc/passwdfile.meebling";
+    "meeblingthedevilish".hashedPasswordFile = "/etc/passwdfile.meeblingthedevilish";
+  };
   hardware.enableRedistributableFirmware = true;
   services = {
     openssh = {
       openFirewall = true;
       enable = true;
     };
-
     kanidm.unixSettings.pam_allowed_login_groups = ["atreus-login"];
   };
 }
