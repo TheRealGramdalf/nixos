ea8ffb2cf96382f754bd4d1d3dc074fce94ee057
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 00:13:09 +0000

    These should probably just be merged tbh

---
+++ config/users/gramdalf/firefox/privacy.nix
@@ -0,0 +1,87 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in {
+  programs.firefox = {
+    policies = {
+      SearchSuggestEnabled = lock-false;
+      FirefoxHome = {
+        Search = true;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+        Locked = false;
+      };
+      FirefoxSuggest = {
+        WebSuggestions = false;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+        Locked = true;
+      };
+      # Ad-hoc user preferences, managed by policy
+      Preferences = {
+        privacy = {
+          donottrackheader = {
+            enabled = lock-true;
+            value = 1;
+            Status = "locked";
+          };
+          firstparty.isolate = lock-true;
+          query_stripping = lock-true;
+          trackingprotection = {
+            cryptomining.enabled = lock-true;
+            enabled = lock-true;
+            fingerprinting.enabled = lock-true;
+            pbmode.enabled = lock-true;
+          };
+          usercontext.about_newtab_segregation.enabled = lock-true;
+        };
+        network.http.speculative-parallel-limit = {
+          Value = 0;
+          Status = "locked";
+        };
+        browser.send_pings = lock-false;
+        browser.places.speculativeConnect.enabled = lock-false;
+        browser.urlbar.speculativeConnect.enabled = lock-false;
+        network = {
+          # DoH (Trusted Recursive Resolver)
+          trr.mode = {
+            Value = 5;
+            Status = "locked";
+          };
+          prefetch-next = lock-false;
+          predictor = {
+            enabled = lock-false;
+            enable-prefetch = lock-false;
+          };
+        };
+        # Crash reports
+        breakpad.reportURL = {
+          Value = "";
+          Status = "locked";
+        };
+        browser.startup.homepage_override.mstone = "ignore";
+        # Not sure if these overlap
+        extensions.getAddons.showPane = lock-false;
+        extensions.htmlaboutaddons.recommendations.enabled = lock-false;
+        datareporting.policy.dataSubmissionEnabled = lock-false;
+        datareporting.healthreport.uploadEnabled = lock-false;
+        toolkit.telemetry.enabled = lock-false;
+      };
+    };
+  };
+}
--- config/users/gramdalf/firefox/telemetry.nix
+++ config/users/gramdalf/firefox/telemetry.nix
@@ -3,12 +3,30 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in {
   programs.firefox = {
     policies = {
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
+      DisableFirefoxStudies = lock-true;
+      DisablePocket = lock-true;
+      DisableTelemetry = lock-true;
+      "UserMessaging" = {
+        WhatsNew = true;
+        ExtensionRecommendations = false;
+        FeatureRecommendations = false;
+        UrlbarInterventions = false;
+        SkipOnboarding = true;
+        MoreFromMozilla = false;
+        Locked = true;
+      };
     };
   };
 }
