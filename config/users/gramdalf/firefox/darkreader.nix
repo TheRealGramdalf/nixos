59a1b88e12feb4c473b5790021721ebbaf788828
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 00:05:35 +0000

    Split darkreader

--- config/users/gramdalf/firefox/darkreader.nix
+++ config/users/gramdalf/firefox/darkreader.nix
@@ -4,16 +4,9 @@
   pkgs,
   ...
 }: {
+  # Make sure to load `dark-reader-settings.json` after first install, since there is (currently) no way of declarative configuration
   programs.firefox = {
     policies = {
-      "3rdparty".Extensions."addon@darkreader.org" = {
-        detectDarkTheme = true;
-        previewNewDesign = true;
-        fetchNews = false;
-        disabledFor = [
-          "*.aer.dedyn.io"
-        ];
-      };
       ExtensionSettings."addon@darkreader.org" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
         installation_mode = "force_installed";
