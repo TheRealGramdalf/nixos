cb1d11c782741fc489159e644425090ccac4bc73
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 22:57:43 +0000

    Add nix.settings

--- config/common/tomeutils.nix
+++ config/common/tomeutils.nix
@@ -12,4 +12,10 @@
     tealdeer # TL;DR for linux commands
     comma # , Run any command from nixpkgs
   ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
