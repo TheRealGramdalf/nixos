638e4eb0d174431618a5ebe9251c4872d1ea5a76
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 00:07:22 +0000

    Hopefully fix ublock

--- config/users/gramdalf/firefox/adblock.nix
+++ config/users/gramdalf/firefox/adblock.nix
@@ -7,19 +7,26 @@
   programs.firefox = {
     policies = {
       "3rdparty".Extensions."uBlock0@raymondhill.net" = {
-        toAdd.filterLists = [
-          "user-filters"
-          "ublock-filters"
-          "ublock-badware"
-          "ublock-privacy"
-          "ublock-abuse"
-          "ublock-unbreak"
-          "easylist"
-          "easyprivacy"
-          "urlhaus-1"
-          "plowe-0"
-          "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
-        ];
+        adminSettings = {
+          userSettings = {
+            importedLists = [
+              "https://github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+            ];
+          };
+          selectedFilterLists = [
+            "user-filters"
+            "ublock-filters"
+            "ublock-badware"
+            "ublock-privacy"
+            "ublock-abuse"
+            "ublock-unbreak"
+            "easylist"
+            "easyprivacy"
+            "urlhaus-1"
+            "plowe-0"
+            "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt"
+          ];
+        };
       };
       ExtensionSettings."uBlock0@raymondhill.net" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
