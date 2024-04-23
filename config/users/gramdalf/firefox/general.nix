42b6081dd7cec51ddcfcf8741eb6f86e959f7d75
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 00:12:54 +0000

    Split even more

---
+++ config/users/gramdalf/firefox/general.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in {
+  programs.firefox = {
+    policies = {
+      AppAutoUpdate = lock-false;
+      # DisableProfileRefresh #?
+      ExtensionSettings = {
+        # Remember to import the default config
+        "treestyletab@piro.sakura.ne.jp" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tree-style-tab/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "menupanel";
+        };
+        "Tab-Session-Manager@sienori" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-session-manager/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "menupanel";
+        };
+      };
+    };
+  };
+}
