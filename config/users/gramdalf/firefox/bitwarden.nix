77ae2adb6fe2048ade713db3f8578500aacaf029
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-23 00:08:16 +0000

    Split bitwarden

---
+++ config/users/gramdalf/firefox/bitwarden.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in {
+  programs.firefox = {
+    policies = {
+      "3rdparty".Extensions."{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+        environment.base = "https://vault.aer.dedyn.io";
+      };
+      ExtensionSettings."{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+        installation_mode = "force_installed";
+        default_area = "navbar";
+      };
+      DisableFormHistory = lock-true;
+      OfferToSaveLogins = lock-false;
+      PasswordManagerEnabled = lock-false;
+    };
+  };
+}
+
