064f6329e41f7cd6f4407e363e2fdb20798ccc73
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-24 21:00:59 +0000

    Remove extra stuff

--- config/users/gramdalf/firefox.nix
+++ config/users/gramdalf/firefox.nix
@@ -55,13 +55,4 @@ in {
       };
     };
   };
-
-  # https://stackoverflow.com/questions/53658303/fetchfromgithub-filter-down-and-use-as-environment-etc-file-source
-
-  #home.file.".mozilla/firefox/gramdalf/chrome".source = pkgs.fetchFromGitHub {
-  #  owner = "soulhotel";
-  #  repo = "FF-ULTIMA";
-  #  rev = "1.6.8";
-  #  hash = "sha256-q+AyJF1cocyh2zWmp0VbLmduLbJqcfKQVbWlHUjCm5A=";
-  #};
 }
