cdc03d088d0a1f23e4cf3149bfd5f076ab807999
TheRealGramdalf <gramdalftech@gmail.com>
2024-04-24 23:07:02 +0000

    Fix aer-files

--- config/hosts/aer-files/main.nix
+++ config/hosts/aer-files/main.nix
@@ -6,12 +6,13 @@
   imports = [
     # Commonly used config
     ../../common/posix-client.nix
-    ../../common/avahi-client.nix
+    ../../common/lan.nix
 
     # Host-specific config
     ./configuration.nix
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
   ];
+  services.chrony.enable = false;
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.05";
