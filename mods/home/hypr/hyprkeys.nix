415a83784e9f75ae5414df42226fec70b8b24ef8
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-08 00:59:48 +0000

    Make hyprkeys functional

--- mods/home/hypr/hyprkeys.nix
+++ mods/home/hypr/hyprkeys.nix
@@ -1,32 +1,41 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, osConfig, ... }:
 let
   inherit (lib) types;
-  inherit (lib) mkEnableOption;
-  inherit (lib) mkOption;
+  inherit (lib) mkEnableOption mkMerge mkOption optionals;
   cfg = config.tomeutils.hyprkeys;
 in {
+  ###### Definition
   options.tomeutils.hyprkeys = {
-    enable = mkEnableOption "Enable selected keybinds" // { default = true; };
+    enable = mkEnableOption "selected keybinds"
+    // { default = (wayland.windowManager.hyprland); };
+
     volume = {
-      enable = mkEnableOption "Enable volume keybinds (Volume +/-, mute)"; # Enable if `osConfig` pipewire
-      backend = mkOption types.oneOf ["wpctl"];
-      steps = mkOption types.int {
+      enable = mkEnableOption "volume keybinds (Volume +/-, mute)"
+      // { default = (osConfig.services.pipewire.enable); };
+      steps = mkOption {
+        type = types.int;
         default = 5;
         description = "How much to step the volume by on each activation";
       };
     };
-    media = {
-      enable = mkEnableOption "Enable media keybinds (Play/Pause, seek track)"; # default true
-      backend = mkOption types.oneOf ["playerctl"];
-    };
+
+    media.enable = mkEnableOption "media keybinds (Play/Pause, seek track), powered by `playerctl`"
+    // { default = (osConfig.services.pipewire.enable); };
+
     brightness = {
-      enable = mkEnableOption "Enable brightness keybinds"; # Enable if `osConfig` brillo?
-      backend = mkOption types.oneOf ["brillo"]; #"brightnessctl"
+      enable = mkEnableOption "brightness keybinds"
+      // { (osConfig.hardware.brillo.enable) };
+      steps = mkOption {
+        type = types.int;
+        default = 5;
+        description = "How much to step the volume by on each activation";
+      };
     };
     screenshot = {
-      enable = mkEnableOption "Enable screenshot (printscreen) keybinds, powered by `grimblast`"; # default true
-      backend = types.oneOf ["grimblast"];
-      args = mkOption types.str {
+      enable = mkEnableOption "screenshot (printscreen) keybinds, powered by `grimblast`"
+      // { default = true; };
+      args = mkOption {
+        type = types.str;
         default = "copysave area --freeze";
         description = "Verbatim args passed to `grimblast`";
       };
@@ -35,24 +44,32 @@ in {
 
   # Implementation
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = cfg.enable -> !config.programs.hyprland.enable;
-        message = "Hyprland must be enabled for `hyprkeys` to work";
-      }
-    ];
     wayland.windowManager.hyprland.settings = {
-      binde = [
-        (lib.lists.optionals cfg.volume.enable ",XF86AudioMute, exec, ${cfg.volume.backend} set-mute @DEFAULT_SINK@ toggle")
-        (lib.lists.optionals cfg.volume.enable ",XF86AudioRaiseVolume, exec, ${cfg.volume.backend} set-volume -l 1.0 @DEFAULT_SINK@ ${cfg.volume.steps}%+")
-        (lib.lists.optionals cfg.volume.enable ",XF86AudioLowerVolume, exec, ${cfg.volume.backend} -l 1.0 @DEFAULT_SINK@ ${cfg.volume.steps}%-")
+      binde = mkMerge [
+        (optionals cfg.volume.enable [
+          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
+          ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ ${builtins.toString cfg.volume.steps}%+"
+          ",XF86AudioLowerVolume, exec, wpctl -l 1.0 @DEFAULT_SINK@ ${builtins.toString cfg.volume.steps}%-"
+        ])
 
-        (lib.lists.optionals cfg.brightness.enable",XF86MonBrightnessUp, exec, ${cfg.brightness.backend} -A ${cfg.brightness.steps}")
-        (lib.lists.optionals cfg.brightness.enable",XF86MonBrightnessDown, exec, ${cfg.brightness.backend} -U ${cfg.brightness.steps}")
+        (optionals cfg.brightness.enable [
+          ",XF86MonBrightnessUp, exec, brillo -A ${builtins.toString cfg.brightness.steps}"
+          ",XF86MonBrightnessDown, exec, brillo -U ${builtins.toString cfg.brightness.steps}"
+        ])
+      ];
+      bind = mkMerge [
+        (optionals cfg.screenshot.enable ["grimblast ${cfg.screenshot.args}"])
+        (optionals cfg.media.enable [
+          ",XF86AudioPrev, exec, playerctl previous"
+          ",XF86AudioNext, exec, playerctl next"
+          ",XF86AudioPlay, exec, playerctl play-pause"
+          ",XF86AudioStop, exec, playerctl play-pause"
+        ])
       ];
     };
-    home.packages = [
-      (lib.lists.optionals cfg.screenshot.enable pkgs.grimblast)
+    home.packages = mkMerge [
+      (optionals cfg.screenshot.enable [pkgs.grimblast])
+      (optionals cfg.media.enable [pkgs.playerctl])
     ];
   };
 }
