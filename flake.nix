ccbdd92370246d3082b4b2c484537701099f0e90
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-08 01:30:14 +0000

    Remove extra

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
   }:
   # Create convenience shorthands
   let
-    #
     inherit (nixpkgs.lib) nixosSystem;
     inherit (home-manager.lib) homeManagerConfiguration;
   in {
@@ -115,12 +114,10 @@
     # See https://nixos.org/manual/nix/stable/command-ref/conf-file.html?highlight=you%20can%20prefix%20the%20name%20of%20the%20setting%20by%20extra-%20to%20append%20to%20the%20previous%20value#file-format
     extra-substituters = [
       "https://nix-community.cachix.org"
-      #"https://hyprland.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }
