c9cbf3c22cc3996cc9a7111ad90a6c5f0d0fea21
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-10 21:04:01 +0000

    Add workspaces, reload

--- mods/home/hypr/hyprkeys.nix
+++ mods/home/hypr/hyprkeys.nix
@@ -7,7 +7,7 @@ in {
   ###### Definition
   options.tomeutils.hyprkeys = {
     enable = mkEnableOption "selected keybinds"
-    // { default = (wayland.windowManager.hyprland); };
+    // { default = (config.wayland.windowManager.hyprland.enable); };
 
     volume = {
       enable = mkEnableOption "volume keybinds (Volume +/-, mute)"
@@ -22,9 +22,19 @@ in {
     media.enable = mkEnableOption "media keybinds (Play/Pause, seek track), powered by `playerctl`"
     // { default = (osConfig.services.pipewire.enable); };
 
+    reload.enable = mkEnableOption "hyprland/waybar reload keybind (ctrl + alt + delete)"
+    // { default = true; };
+
+    workspaces = {
+      enable = mkEnableOption "basic workspace keybinds, based on the hyprland starter config"
+      // { default = true; };
+      scratchpad.enable = mkEnableOption "a drop-down style special workspace"
+      // { default = false; };
+    };
+
     brightness = {
       enable = mkEnableOption "brightness keybinds"
-      // { (osConfig.hardware.brillo.enable) };
+      // { default = (osConfig.hardware.brillo.enable); };
       steps = mkOption {
         type = types.int;
         default = 5;
@@ -45,6 +55,9 @@ in {
   # Implementation
   config = lib.mkIf cfg.enable {
     wayland.windowManager.hyprland.settings = {
+      # Set the mainmod in case it isn't elsewhere
+      "$mainMod" = lib.mkDefault "SUPER";
+      # bind[r]e[peat]
       binde = mkMerge [
         (optionals cfg.volume.enable [
           ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
@@ -64,6 +77,51 @@ in {
           ",XF86AudioNext, exec, playerctl next"
           ",XF86AudioPlay, exec, playerctl play-pause"
           ",XF86AudioStop, exec, playerctl play-pause"
+        ])
+        (optionals cfg.reload.enable [
+          # Reload hyprland
+          "CTRL + ALT, delete, exec, hyprctl reload && systemctl restart --user waybar hypridle"
+        ])
+        (optionals cfg.workspaces.enable [
+          # Switch workspaces with mainMod + [0-9]
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
+          # Move active window to a workspace with mainMod + SHIFT + [0-9]
+          "$mainMod SHIFT, 1, movetoworkspace, 1"
+          "$mainMod SHIFT, 2, movetoworkspace, 2"
+          "$mainMod SHIFT, 3, movetoworkspace, 3"
+          "$mainMod SHIFT, 4, movetoworkspace, 4"
+          "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
+        ])
+        (optionals cfg.workspaces.scratchpad.enable [
+          # Special workspace (scratchpad)
+          "$mainMod, S, togglespecialworkspace, magic"
+        ])
+      ];
+      bindm = mkMerge [
+        (optionals cfg.workspaces.enable [
+          # Scroll through existing workspaces with mainMod + scroll
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+        ])
+      ];
+      animations.animation = mkMerge [
+        (optionals cfg.workspaces.scratchpad.enable [
+        # Make special workspaces (scratchpad) slide in vertically
+        "specialWorkspace, 1, 6, default, slidefadevert"
         ])
       ];
     };
