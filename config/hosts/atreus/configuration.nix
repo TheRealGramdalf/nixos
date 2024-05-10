1297febfde1fc9921c8052756de7225c46051428
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-10 22:50:03 +0000

    Fix ssh keys

--- config/hosts/atreus/configuration.nix
+++ config/hosts/atreus/configuration.nix
@@ -1,4 +1,7 @@
 {
+  users.users."root".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5ibKzd+V2eR1vmvBAfSWcZmPB8zUYFMAN3FS6xY9ma"
+  ];
   hardware.enableRedistributableFirmware = true;
   services = {
     openssh = {
