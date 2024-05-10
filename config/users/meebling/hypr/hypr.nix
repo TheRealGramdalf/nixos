0a1c5f158db6952ef9770bff76f9054193d1386c
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-10 21:10:17 +0000

    Remove extras

--- config/users/meebling/hypr/hypr.nix
+++ config/users/meebling/hypr/hypr.nix
@@ -7,10 +7,7 @@
     ./hyprlock.nix
     ./hypridle.nix
   ];
-  home.packages = with pkgs; [
-    playerctl
-    grimblast
-  ];
+  
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
@@ -21,8 +18,6 @@
       "$terminal" = "kitty";
       "$fileManager" = "nautilus";
       "$menu" = "anyrun";
-      # Bind prefs
-      "$mainMod" = "SUPER";
 
       monitor = [
         # name,resolution,position,scale
@@ -71,59 +66,6 @@
         "$mainMod, right, movefocus, r"
         "$mainMod, up, movefocus, u"
         "$mainMod, down, movefocus, d"
-
-        # Switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        # Example special workspace (scratchpad)
-        "$mainMod, S, togglespecialworkspace, magic"
-
-        # Scroll through existing workspaces with mainMod + scroll
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-
-        # Enable XF86 (media) keys
-        #",XF86AudioMedia, exec, " # Not sure what this one does
-        ",XF86AudioPrev, exec, playerctl previous"
-        ",XF86AudioNext, exec, playerctl next"
-        ",XF86AudioPlay, exec, playerctl play-pause"
-        ",XF86AudioStop, exec, playerctl play-pause"
-
-        # Screenshot
-        ", Print, exec, grimblast copysave area"
-
-        # Reload hyprland
-        "CTRL + ALT, delete, exec, hyprctl reload && systemctl restart --user waybar hypridle"
-      ];
-      # bind[r]e[peat]
-      binde = [
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%-"
-
-        ",XF86MonBrightnessUp, exec, brillo -A 5"
-        ",XF86MonBrightnessDown, exec, brillo -U 5"
       ];
       bindm = [
         # Move/resize windows with mainMod + LMB/RMB and dragging
@@ -140,8 +82,6 @@
           "borderangle, 1, 8, default"
           "fade, 1, 7, default"
           "workspaces, 1, 6, default"
-          # Make special workspaces (scratchpad) slide in vertically
-          "specialWorkspace, 1, 6, default, slidefadevert"
         ];
       };
       decoration = {
@@ -173,8 +113,6 @@
         "col.active_border" = "$mauve";
         "col.inactive_border" = "$surface0";
         layout = "dwindle";
-        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-        allow_tearing = false;
       };
       env = [
         "QT_QPA_PLATFORMTHEME,qt5ct" # change to qt6ct if you have that
