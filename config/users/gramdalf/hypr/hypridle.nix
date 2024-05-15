9a7c6387b1764deb7e925dc558a7677e69399e24
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 23:17:49 +0000

    hypr*

--- config/users/gramdalf/hypr/hypridle.nix
+++ config/users/gramdalf/hypr/hypridle.nix
@@ -10,16 +10,17 @@
         # Avoid running hyprlock twice
         lock_cmd = "hyprctl dispatch exec 'pidof hyprlock || hyprlock'"; # Commands to run when `loginctl lock-session` is called
         #unlock_cmd = notify-send "unlock!"      # same as above, but unlock
+        # Use this to debug sleep
         before_sleep_cmd = "hyprctl dispatch exec 'loginctl lock-session'";    # command ran before sleep
         #after_sleep_cmd = "";  # command ran after sleep
         ignore_dbus_inhibit = false;  # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
       };
 
-      #listener {
-      #  timeout = 500                            # in seconds
-      #  on-timeout = notify-send "You are idle!" # command to run when timeout has passed
-      #  on-resume = notify-send "Welcome back!"  # command to run when activity is detected after timeout has fired.
-      #}
+      listener = [
+        {timeout = 300; # in seconds
+        on-timeout = "hyprctl dispatch exec 'loginctl lock-session'"; # command to run when timeout has passed
+        on-resume = "";}# command to run when activity is detected after timeout has fired.
+      ];
     };
   };
   systemd.user.services."hypridle".Service.Environment = ["PATH=$PATH:${lib.makeBinPath [pkgs.hyprland]}"];
