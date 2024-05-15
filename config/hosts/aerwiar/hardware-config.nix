4e84cd10cb96b664cc0e2c7d595245b3e80f52e7
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 23:12:59 +0000

    Remove networking.useDHCP

--- config/hosts/aerwiar/hardware-config.nix
+++ config/hosts/aerwiar/hardware-config.nix
@@ -41,16 +41,6 @@
     neededForBoot = false;
   };
 
-  swapDevices = [];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
