0c7cd6c527fdb9908e66ce9f851a462909af48d4
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 20:18:12 +0000

    Add a polkit agent

--- config/common/hypr/hypr.nix
+++ config/common/hypr/hypr.nix
@@ -24,19 +24,17 @@
     MOZ_ENABLE_WAYLAND = "1";
   };
 
-  systemd = {
-  user.services."polkit-agent" = {
-    description = "polkit-kde-agent for Hyprland";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit-kde-agent}/libexec/polkit-kde-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
+  systemd.user.services."polkit-agent" = {
+  description = "polkit-kde-agent for Hyprland";
+  wantedBy = [ "graphical-session.target" ];
+  wants = [ "graphical-session.target" ];
+  after = [ "graphical-session.target" ];
+  serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
   };
 
