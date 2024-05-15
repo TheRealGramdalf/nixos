d6bab1878bed8ac9fc55eca55046408343008a4e
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 23:33:00 +0000

    Fix networking stuff

--- config/hosts/atreus/hardware-config.nix
+++ config/hosts/atreus/hardware-config.nix
@@ -48,8 +48,6 @@
     neededForBoot = false;
   };
 
-  networking.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
