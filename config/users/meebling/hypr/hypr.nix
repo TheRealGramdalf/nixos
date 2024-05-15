584f311f849fab79f07c9fc9a6739b8f618470a7
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 20:17:29 +0000

    user preferences

--- config/users/meebling/hypr/hypr.nix
+++ config/users/meebling/hypr/hypr.nix
@@ -53,7 +53,7 @@
       bind = [
         "$mainMod, SPACE, exec, $menu"
         "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive,"
+        "ALT, F4, killactive,"
         "$mainMod, M, exit,"
         "$mainMod, E, exec, $fileManager"
         "$mainMod, V, togglefloating,"
