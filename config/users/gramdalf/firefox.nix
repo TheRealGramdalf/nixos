c2639b0801c209d40b55da19f6dbb48de6f68054
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 23:13:09 +0000

    Fix firefox

--- config/users/gramdalf/firefox.nix
+++ config/users/gramdalf/firefox.nix
@@ -1,13 +1,4 @@
-{...}: let
-  lock-false = {
-    Value = false;
-    Status = "locked";
-  };
-  lock-true = {
-    Value = true;
-    Status = "locked";
-  };
-in {
+{...}: {
   imports = [
     ./firefox/bitwarden.nix
     ./firefox/adblock.nix
@@ -29,25 +20,19 @@ in {
       search.force = true;
       settings = {
         # These privacy settings can't be set via policy, so they need to be set here instead
-        "datareporting.healthreport.uploadEnabled" = lock-false;
-        "toolkit.telemetry.enabled" = lock-false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.enabled" = false;
         # Crash reports
-        "breakpad.reportURL" = {
-          Value = "";
-          Status = "locked";
-        };
-        "privacy.donottrackheader.enabled" = lock-true;
-        "privacy.donottrackheader" = {
-          Value = 1;
-          Status = "locked";
-        };
-        "privacy.firstparty.isolate" = lock-true;
-        "privacy.query_stripping" = lock-true;
-        "privacy.trackingprotection.enabled" = lock-true;
-        "privacy.trackingprotection.cryptomining.enabled" = lock-true;
-        "privacy.trackingprotection.fingerprinting.enabled" = lock-true;
-        "privacy.trackingprotection.pbmode.enabled" = lock-true;
-        "usercontext.about_newtab_segregation.enabled" = lock-true;
+        "breakpad.reportURL" = " ";
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.donottrackheader" = "1";
+        "privacy.firstparty.isolate" = true;
+        "privacy.query_stripping" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.cryptomining.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.pbmode.enabled" = true;
+        "privacy.usercontext.about_newtab_segregation.enabled" = true;
       };
     };
   };
