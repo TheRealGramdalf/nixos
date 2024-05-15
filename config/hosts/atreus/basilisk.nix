7eba524bd7824956daf6a92f3bdba73bc49029f4
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-15 18:42:01 +0000

    Add basilisk

---
+++ config/hosts/atreus/basilisk.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  hardware.openrazer = {
+    enable = true;
+    users = [
+      "meebling"
+      "meeblingthedevilish"
+    ];
+  };
+  environment.systemPackages = [
+    pks.razergenie
+  ]
+}
