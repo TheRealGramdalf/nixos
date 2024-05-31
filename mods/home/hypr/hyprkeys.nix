0d744a1dae07875cfd82c7a417f8ef6fc77e9df7
TheRealGramdalf <gramdalftech@gmail.com>
2024-05-31 16:10:55 +0000

    Fix keybinds

--- mods/home/hypr/hyprkeys.nix
+++ mods/home/hypr/hyprkeys.nix
@@ -137,8 +137,6 @@ in {
           "$mainMod SHIFT, 8, movetoworkspace, 8"
           "$mainMod SHIFT, 9, movetoworkspace, 9"
           "$mainMod SHIFT, 0, movetoworkspace, 10"
-        ];
-        bindm = [
           # Scroll through existing workspaces with mainMod + scroll
           "$mainMod, mouse_down, workspace, e+1"
           "$mainMod, mouse_up, workspace, e-1"
