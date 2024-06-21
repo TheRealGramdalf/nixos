5132a5c289f79850f62ede9d0c74404c3dc6af75
TheRealGramdalf <gramdalftech@gmail.com>
2024-06-21 21:08:47 +0000

    aer: starter network configuration

---
+++ config/hosts/aer/networking.nix
@@ -0,0 +1,58 @@
+{config, ...}: 
+let
+  eno1 = "enp1s0f0"; # Double check this
+  eno2 = "enp1s0f1"; # ^
+  eno3 = "enp2s0f0";
+  eno4 = "enp2s0f1";
+in {
+  networking = {
+    useNetworkd = true;
+    useDHCP = false;
+    # dhcpcd is far inferior to systemd-networkd
+    dhcpcd.enable = false;
+  };
+  systemd.network = {
+    # wait-online might be useful if a single interface can be used
+    wait-online.enable = false;
+    network.enable = true;
+    networks = {
+      "10-ingress" = {
+        name = "${eno1}";
+        gateway = "192.168.1.1";
+        address = ["192.168.1.5/24"];
+      };
+      "14-recovery" = {
+        # Ad-hoc recovery interface running a DHCP server
+        name = "${eno4}";
+        address = ["10.0.0.1/24"];
+        networkConfig.DHCPServer = true;
+      };
+    };
+  };
+  services = {
+    resolved = {
+      llmnr = false;
+      enable = true;
+      fallbackDns = [
+        "1.1.1.1"
+        "1.0.0.1"
+      ];
+      extraConfig = ''
+        [Resolve]
+        MulticastDNS = false
+      '';
+    };
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+      allowInterfaces = ["${eno1}"];
+    };
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+      interface = "${eno1}";
+    };
+    chrony.enable = true;
+  };
+}
