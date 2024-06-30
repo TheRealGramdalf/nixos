8fbc870225ba75fb7d6822b5c6a24f434a80a85c
TheRealGramdalf <gramdalftech@gmail.com>
2024-06-30 23:38:39 +0000

    fix database location, define unixSettings

--- config/hosts/aer/services/kanidm.nix
+++ config/hosts/aer/services/kanidm.nix
@@ -2,18 +2,30 @@
 let
   cfg = config.services.kanidm;
   name = "kanidm";
+  dataDir = "/persist/services/kanidm";
 in {
+  # Temporary fix for nixpkgs#323674
+  systemd.services."kanidm".environment."KANIDM_DB_PATH" = "${dataDir}/db/kanidm.db";
   services.kanidm = {
     enableServer = true;
     enablePam = true;
     serverSettings = {
-      log_level = "DEBUG";
+      log_level = "debug";
       origin = "https://auth.aer.dedyn.io";
       domain = "auth.aer.dedyn.io";
-      db_path = "/persist/services/kanidm/kanidm.db";
+      # Needs https://github.com/NixOS/nixpkgs/pull/323676 to work properly
+      #db_path = "${dataDir}/db/kanidm.db";
+      db_fs_type = "zfs";
+      tls_chain = "${dataDir}/chain.pem";
+      tls_key = "${dataDir}/key.pem";
       bindaddress = "127.0.0.1:8443";
     };
     clientSettings.uri = cfg.serverSettings.origin;
+    unixSettings = {
+      pam_allowed_login_groups = [];
+      home_attr = "uuid";
+      home_alias = "spn";
+    };
   };
   
   services.caddy.virtualHosts."${cfg.serverSettings.domain}" = {
