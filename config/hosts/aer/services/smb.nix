e106e9353303ed64f1d04db253a22cd79a797c9f
TheRealGramdalf <gramdalftech@gmail.com>
2024-06-30 01:24:18 +0000

    aer: format smb.nix

--- config/hosts/aer/services/smb.nix
+++ config/hosts/aer/services/smb.nix
@@ -10,18 +10,13 @@
       shares = {
         photos = {
           path = "/tank/photos";
-          writable = "true";
+          writable = true;
           comment = "Ye Olde Photos";
         };
         media = {
           path = "/tank/media";
           writable = true;
           comment = "The Seven Seas";
-        };
-        nixos = {
-          path = "/etc/nixos";
-          writable = true;
-          comment = "NixOS Configurations";
         };
         Data = {
           path = "/tank/Data";
@@ -35,8 +30,9 @@
         inherit owner = unix only
         inherit permissions = yes
         # ^^ Overrides `create` and `force create` `mask/mode`
+
         # Authentication
-              passdb backend = tdbsam:/tank/samba-passdb.tdb
+        passdb backend = tdbsam:/tank/samba-passdb.tdb
         security = user
         hosts allow = 192.168.1. 127.0.0.1
         hosts deny = ALL
@@ -44,10 +40,12 @@
         map to guest = Bad User
         # guest ok = true
         #//TODO Look into: `invalid users`
+
         # Generic
         server smb encrypt = required
         # ^^ Note: Breaks `smbclient -L <ip> -U%`
         server min protocol = SMB3_00
+
         ## Performance Optimizations & platform compatibility
         use sendfile = yes
       '';
