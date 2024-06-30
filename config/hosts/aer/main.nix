fb5cd7d1b55df672e21d50c4d142fef8be799cee
TheRealGramdalf <gramdalftech@gmail.com>
2024-06-30 23:37:42 +0000

    aer: fix imports

--- config/hosts/aer/main.nix
+++ config/hosts/aer/main.nix
@@ -1,11 +1,13 @@
 {config, ...}: {
   imports = [
-    ./docker.nix
+    #./docker.nix
     ./networking.nix
+    ./hardware.nix
     
     # Services
     ./services/services.nix
   ];
+  nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.05";
   networking = {
