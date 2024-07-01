ad125a04efd2b8f92b27518b3091295c30e3d2b4
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-01 00:20:05 +0000

    aer: open firewall

--- config/hosts/aer/services/caddy.nix
+++ config/hosts/aer/services/caddy.nix
@@ -1,12 +1,15 @@
 _: {
   services.caddy = {
     enable = true;
-    user = "";
-    group = "";
+    # Caddy's user/group must be local, since it's required for `kanidm-unixd` to function properly
     dataDir = "/persist/services/caddy";
     globalConfig = ''
-      #debug
+      debug
       grace_period 10s
     '';
   };
+  networking.firewall = {
+    allowedUDPPorts = [80 443];
+    allowedTCPPorts = [80 443];
+  }
 }
