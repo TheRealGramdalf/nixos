10fca58dd73af2537397c5960994895ffbb6489d
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-05 23:07:18 +0000

    iso: add git

--- flake.nix
+++ flake.nix
@@ -171,7 +171,10 @@
             imports = [
               "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
             ];
-            environment.systemPackages = [ pkgs.neovim ];
+            environment.systemPackages = with pkgs; [
+              neovim
+              git 
+            ];
             users.users."root".openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5ibKzd+V2eR1vmvBAfSWcZmPB8zUYFMAN3FS6xY9ma"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKkR0w0kYy8ad6ulnF9o7ULZXOMy7kOdoxXzTEi5dqcq"
