1eba54a2b6e2b491a966ce703bab0ef8cc672196
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-09 06:40:42 +0000

    aer: enable docker

--- config/hosts/aer/docker.nix
+++ config/hosts/aer/docker.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
+  virtualisation.oci-containers.backend = "docker";
   virtualisation.docker = {
     enable = true;
     package = pkgs.docker_26;
     storageDriver = "zfs";
     liveRestore = true;
   };
+  environment.systemPackages = [pkgs.docker-compose];
 }
