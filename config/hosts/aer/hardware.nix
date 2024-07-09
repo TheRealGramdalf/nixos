2fa3540d0c3b62e64de0d3c2a0d8b19874f947a7
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-09 19:41:49 +0000

    aer: enable all firmware (for broadcom NIC)

--- config/hosts/aer/hardware.nix
+++ config/hosts/aer/hardware.nix
@@ -7,6 +7,7 @@ _: {
     ];
   };
 
+  hardware.enableAllFirmware = true;
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ata_piix" "mpt3sas" "mvsas" "mptsas" "usb_storage" "usbhid" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
 
