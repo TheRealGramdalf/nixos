a73a858ab8dfc084c1af0fe43f0a3336c735f816
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-09 19:57:59 +0000

    aer: import modules, fix ssh

--- flake.nix
+++ flake.nix
@@ -139,6 +139,7 @@
       "aer" = nixosSystem {
         modules = [
           ./config/hosts/aer/main.nix
+          ./mods/nixos/main.nix
         ];
       };
       "aer-test" = nixosSystem {
@@ -198,20 +199,5 @@
         ];
       };
     };
-  };
-
-  # Global nix.settings, these are presented to the user as an optional choice on rebuild
-  nixConfig = {
-    # Enable rebuilding with flakes
-    experimental-features = ["nix-command" "flakes"];
-    # Add the nix-community binary cache to make builds faster
-    # See https://nixos.org/manual/nix/stable/command-ref/conf-file.html?highlight=you%20can%20prefix%20the%20name%20of%20the%20setting%20by%20extra-%20to%20append%20to%20the%20previous%20value#file-format
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 }
