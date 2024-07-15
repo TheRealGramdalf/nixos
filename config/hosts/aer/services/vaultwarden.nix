f7c2c800625d779956c60afb0acaaded655e7d46
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-15 22:23:25 +0000

    aer: remove excess dependencies

--- config/hosts/aer/services/vaultwarden.nix
+++ config/hosts/aer/services/vaultwarden.nix
@@ -31,13 +31,9 @@ in {
   systemd.services."vaultwarden" = {
     after = [
       config.systemd.services."kanidm-unixd".name
-      #config.systemd.services."kanidm".name
-      #config.systemd.services."traefik".name
     ];
     requires = [
       config.systemd.services."kanidm-unixd".name
-      #config.systemd.services."kanidm".name
-      #config.systemd.services."traefik".name
     ];
     serviceConfig = {
       User = lib.mkForce "5fa90349-b863-4d5e-b6c6-5a6f303fdb15";
