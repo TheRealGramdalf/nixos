35e6e959c0963a3dcfb77f55d7af5d2fe504d965
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-18 00:35:38 +0000

    mods: fix traefik module description

--- mods/nixos/cone.nix
+++ mods/nixos/cone.nix
@@ -25,43 +25,26 @@
     if cfg.useEnvSubst == false
     then staticFile
     else "/run/traefik/config.json";
-
-  configNote = ''
-    ::: {.note}
-    Traefik config is serialized to JSON, which is considered valid YAML since 1.2
-    :::
-  '';
 in {
   options.services.cone = {
     # TODO
-    # - [x] inherit lib.types, remove `with`
-    # - [x] Figure out dynamic folder logic/assertions
+    # - [] Rather than using `let`, set `cfg.dynamic.file` to writeText toJSON
     # - [] Add "virtualHosts" convenience feature?
-    # - [x] Remove `config` from static/dynamic
     # - [] Fix defaultText, example (replace with `'' ''`), etc.
-    # - [?] Fix tmpfiles rules, readWritePaths
-    # - [x] Follow caddy for user creation (don't assume it's always created)
-    # - [x] Use statix?
     # - [] If using virtualHost, make it top level (services.traefik.virthosts), and make it a convenience function. Make it assert conflictions with dynamic file
     # - [] Replace default text with clear logic examples
-    # - [x] check rfc 42 https://github.com/NixOS/rfcs/blob/master/rfcs/0042-config-option.md
-    # - [x] Make virtualhost use a different mechanism (tmpfiles?) to create dynamic config, so that the systemd service doesn't need to be restarted (the service definition isn't changed)
-    #   if simply using `nixos-rebuild switch` (for minimal downtime).
-    # - [x] Check docker, see if running a standalone binary works with the docker backend. If not, note the pitfalls
     # - [] Add descr. for options used by the module
     # - [] nixos Tests?
-    # - [] Package pinning?
     # - [] Configuration test via https://github.com/traefik/traefik/issues/10804 (need to grab schema from traefik pkg, add schema output to traefik pkg)
-    # - [x] Remove `createHome` and `homeDir`, use systemd tmpfiles instead
-    # - [x] Use `note` syntax
     # - [] https://github.com/NixOS/nixpkgs/blob/6fd558c210c90f72c8116a0ea509f4280356d2bb/nixos/modules/services/web-servers/caddy/default.nix#L345C5-L345C63
-    # - [x] Use `supplementaryGroups` for docker, since otherwise files managed by the `tmpfiles` rules are owned by group docker
-    # - [] Assertion for extraFiles and traefik.enable
     # CHANGELOG
     # - Removed usage of `with`, replacing it with `inherit` instead
     # - Do not always assume the systemd user must be created by NixOS
     # - Rename options to be more ergonomic and in line with the spirit of RFC 42
     # - (minimal downtime)
+    # - Added `mkRenamedOptionModule`s where appropriate
+    # - If created, the `traefik` users home directory is no longer created
+    # - Increase UDP buffer sizes similar to caddy
     enable = mkEnableOption "Traefik web server";
     static = {
       file = mkOption {
@@ -70,12 +53,19 @@ in {
         type = nullOr path;
         description = ''
           Path to traefik's static configuration to use.
-          (Using this option has precedence over `staticOptions` and `dynamicOptions`)
+
+          ::: {.note}
+          Using this option has precedence over {option}`services.cone.static.settings`
+          :::
         '';
       };
       settings = mkOption {
         description = ''
           Static configuration for Traefik, written in nix
+
+          ::: {.note}
+          This will be serialized to JSON (which is considered valid YAML) at build, and passed to Traefik as `--configfile`
+          :::
         '';
         type = jsonValue;
         default = {entryPoints.http.address = ":80";};
@@ -105,7 +95,10 @@ in {
         type = nullOr path;
         description = ''
           Path to traefik's dynamic configuration to use.
-          (Using this option has precedence over `settings`)
+
+          ::: {.note}
+          Using this option has precedence over {option}`services.cone.dynamic.settings`
+          :::
         '';
       };
       dir = mkOption {
@@ -114,6 +107,7 @@ in {
         type = nullOr path;
         description = ''
           Path to the directory traefik should watch.
+
           ::: {.warning}
           Files in this directory matching the glob _nixos-* (reserved for extraFiles) will be deleted as part of
           systemd-tmpfiles-resetup.service, _**regardless of their origin.**_
@@ -124,6 +118,10 @@ in {
       settings = mkOption {
         description = ''
           Dynamic configuration for Traefik, written in nix.
+
+          ::: {.note}
+          This will be serialized to JSON (which is considered valid YAML) at build, and passed as part of the static file
+          :::
         '';
         type = jsonValue;
         default = {};
@@ -145,13 +143,11 @@ in {
             type = jsonValue;
             description = "Dynamic configuration for Traefik, written in nix.";
             example = {
-              http.routers."api".service = "api@internal";
-              http.services."api".loadbalancer.server.port = 8080;
+              http.routers."api" = {
+                service = "api@internal";
+                rule = "Host(`localhost`)";
+              };
             };
-          };
-          options.name = mkOption {
-            type = path;
-            description = "The mount point of the directory inside the virtual machine";
           };
         });
       default = {};
@@ -164,11 +160,12 @@ in {
         };
       };
       description = ''
-        Extra dynamic configuration files to write. These are placed in `cfg.dynamic.dir` upon activation,
+        Extra dynamic configuration files to write. These are symlinked in `cfg.dynamic.dir` upon activation,
         allowing configuration to be upated without restarting the primary daemon.
+
         ::: {.note}
-        Due to [a limitation in Traefik](); any syntax error in a dynamic configuration will cause the entire file provider to be ignored.
-        This may cause interuption in service, which may include access to the traefik dashboard, if enabled and configured to use [traefik-ception]().
+        Due to [a limitation in Traefik](https://github.com/traefik/traefik/issues/10890); any syntax error in a dynamic configuration will cause the entire file provider to be ignored.
+        This may cause interuption in service, which may include access to the traefik dashboard, if enabled and configured to use [traefik-ception](https://doc.traefik.io/traefik/operations/dashboard/#secure-mode).
         :::
       '';
     };
@@ -206,7 +203,7 @@ in {
       type = str;
       description = ''
         Primary group under which traefik runs.
-        For the docker backend, prefer {option}supplementaryGroups
+        For the docker backend, prefer {option}`services.cone.supplementaryGroups`
 
         ::: {.note}
         If left as the default value this group will automatically be created
@@ -227,11 +224,6 @@ in {
         ::: {.note}
         With the `docker` provider, traefik manages connection to containers via the docker socket,
         which requires membership of the `docker` group for write access 
-        :::
-        ::: {.note}
-        If left as the default value this group will automatically be created
-        on system activation, otherwise you are responsible for
-        ensuring the group exists before the traefik service starts.
         :::
       '';
     };
@@ -254,7 +246,7 @@ in {
         The traefik static configuration methods (env, CLI, and file) are mutually exclusive.
         Rather than setting secret values with the traefik environment variable syntax,
         it is recommended to set arbitray environment variables, then reference them with `$VARNAME` in e.g.
-        {option}static.settings. 
+        {option}`services.cone.static.settings`. 
         :::
       '';
     };
@@ -264,7 +256,7 @@ in {
       type = bool;
       example = true;
       description = ''
-        Whether to use [`envSubst`]() in the ExecStartPre phase to augment the generated static config. See {environmentFiles}
+        Whether to use [`envSubst`]() in the ExecStartPre phase to augment the generated static config. See {option}`services.cone.environmentFiles`
       '';
     };
 
@@ -293,6 +285,10 @@ in {
       }
     ];
 
+    # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
+    boot.kernel.sysctl."net.core.rmem_max" = mkDefault 2500000;
+    boot.kernel.sysctl."net.core.wmem_max" = mkDefault 2500000;
+
     services.cone.static.settings = mkIf (cfg.dynamic.dir != null || cfg.dynamic.file != null) {
       providers.file = {
         directory = mkIf (cfg.dynamic.dir != null) cfg.dynamic.dir;
@@ -316,7 +312,7 @@ in {
             ${getExe pkgs.envsubst} -i "${staticFile}" > "${finalStaticFile}"
           '');
         ExecStart = "${getExe cfg.package} --configfile=${finalStaticFile}";
-        Type = "simple"; # Should this also be notify/notify-reload?
+        Type = "simple";
         User = cfg.user;
         Group = cfg.group;
         SupplementaryGroups = mkIf (cfg.supplementaryGroups != []) cfg.supplementaryGroups;
