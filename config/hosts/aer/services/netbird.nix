c58d4e25fe67a599489aa100dd96ceebe917484a
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-22 21:19:54 +0000

    hosts/aer: enable both authAudience

--- config/hosts/aer/services/netbird.nix
+++ config/hosts/aer/services/netbird.nix
@@ -22,7 +22,7 @@ in {
       enableNginx = true;
       #domain = cfg.domain;
       settings = {
-        #AUTH_AUDIENCE = "netbird";
+        AUTH_AUDIENCE = "netbird";
         AUTH_AUTHORITY = "https://auth.aer.dedyn.io/oauth2/openid/${cid}";
         AUTH_CLIENT_ID = cid;
         AUTH_SUPPORTED_SCOPES = "openid profile email offline_access api";
