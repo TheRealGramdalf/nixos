2e11e14e6f5c408e544329fa534ecc2ef5d8e886
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-24 00:02:59 +0000

    hosts/aerwiar: add netbird tunnels

--- config/hosts/aerwiar/netbird.nix
+++ config/hosts/aerwiar/netbird.nix
@@ -1,8 +1,18 @@
 {pkgs, ...}: {
   services.netbird = {
     enable = true;
+    tunnels = {
+      "home".environment = {
+        NB_ADMIN_URL="https://vpn.aer.dedyn.io";
+        NB_MANAGEMENT_URL="https://vpn.aer.dedyn.io";
+      };
+    };
   };
   environment.systemPackages = [
     pkgs.netbird-ui
   ];
+  environment.sessionVariables = {
+    NB_ADMIN_URL="https://vpn.aer.dedyn.io";
+    NB_MANAGEMENT_URL="https://vpn.aer.dedyn.io";
+  };
 }
