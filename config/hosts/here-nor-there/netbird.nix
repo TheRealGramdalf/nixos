68b6ceb4c8d3dc53c4703dcc531c25456ece64dd
TheRealGramdalf <gramdalftech@gmail.com>
2024-07-25 20:59:58 +0000

    hosts/here-nor-there: remove UI package

--- config/hosts/here-nor-there/netbird.nix
+++ config/hosts/here-nor-there/netbird.nix
@@ -1,8 +1,5 @@
-{pkgs, ...}: {
+{...}: {
   services.netbird.enable = true;
-  environment.systemPackages = [
-    pkgs.netbird-ui
-  ];
   environment.sessionVariables = {
     NB_ADMIN_URL="https://vpn.aer.dedyn.io";
     NB_MANAGEMENT_URL="https://vpn.aer.dedyn.io";
