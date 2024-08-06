70c3e17d006a1f16f30f7480354f6c63ae5afe97
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-06 22:15:18 +0000

    pkgs/dashy-ui: remove fluff, try direct settings

--- config/hosts/aer/services/dashy.nix
+++ config/hosts/aer/services/dashy.nix
@@ -1,35 +1,7 @@
 {context, ...}: let
   domain = "aer.dedyn.io";
   port = 6941;
-  settings = {
-    pageInfo = {
-      title = "Aerwiar";
-      description = "Launchpad for the Aerwiar home server";
-    };
-    appConfig = {
-      preventWriteToDisk = true;
-      disableUpdateChecks = true;
-    };
-  };
 in {
-  #virtualisation.oci-containers."dash" = {
-  #  image = "lissy93/dashy:3.1.1";
-  #  environment = {
-  #    NODE_ENV = "production";
-  #    SSL_PRIV_KEY_PATH = "/etc/secrets/dashy.key";
-  #    SSL_PUB_KEY_PATH = "/etc/secrets/dashy.pub";
-  #    SSL_PORT = 8443;
-  #    UID = 30300;
-  #    GID = 30300; # dashy-aer@auth.aer.dedyn.io
-  #  };
-  #  labels = {
-  #    "traefik.enable" = true;
-  #    "traefik.http.services.dash.loadbalancer.server.scheme" = "https";
-  #    "traefik.http.services.dash.loadbalancer.server.port" = 8443;
-  #    "traefik.http.routers.dash.middlewares" = "local-only@file";
-  #    "traefik.http.routers.dash.rule" = "Host(`aer.dedyn.io`)";
-  #  };
-  #};
   services.nginx = {
     enable = true;
     virtualHosts."${domain}" = {
@@ -37,23 +9,25 @@ in {
         {
           addr = "127.0.0.1";
           inherit port;
-          #proxyProtocol = true;
         }
       ];
       locations = {
         "/" = {
-          root = context.self.packages.x86_64-linux.dashy-ui.override {inherit settings;};
-          tryFiles = "$uri /index.html "; #$uri.html =404
+          root = context.self.packages.x86_64-linux.dashy-ui.override {
+            settings = {
+              pageInfo = {
+                title = "Aerwiar";
+                description = "Launchpad for the Aerwiar home server";
+              };
+              appConfig = {
+                preventWriteToDisk = true;
+                disableUpdateChecks = true;
+              };
+            };
+          };
+          tryFiles = "$uri /index.html ";
         };
-
-        #"/404.html".extraConfig = ''
-        #  internal;
-        #'';
       };
-
-      #extraConfig = ''
-      #  error_page 404 /404.html;
-      #'';
     };
   };
   services.cone.extraFiles."dashy".settings = {
