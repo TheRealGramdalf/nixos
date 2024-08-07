e147c53e2f612be8488e1aaff024dc34b80ead5a
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-07 23:01:40 +0000

    hosts/aer: don't use etc overlay (currently unstable)

--- config/hosts/aer/system.nix
+++ config/hosts/aer/system.nix
@@ -1,6 +1,6 @@
 {...}: {
   boot.initrd.systemd.enable = true;
-  system.etc.overlay.enable = true;
+  system.etc.overlay.enable = false;
   users.mutableUsers = false;
   users.users."root".hashedPasswordFile = "/persist/secrets/root.pw";
 }
