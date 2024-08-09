1bbd588e5816203e80b02e011ed4913c5d0fc0a6
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-09 07:21:25 +0000

    hosts/aer: semi working, enable verbose logging

--- config/hosts/aer/services/netbird/coturn.nix
+++ config/hosts/aer/services/netbird/coturn.nix
@@ -47,9 +47,10 @@ in {
     no-tcp-relay = true;
     no-tls = true;
     no-dtls = true;
-    #extraConfig = ''
-    #  external-ip = 70.70.36.117/192.168.1.5
-    #'';
+    extraConfig = ''
+      verbose = true
+      #external-ip = 70.70.36.117/192.168.1.5
+    '';
   };
 
   services.cone.extraFiles = {
