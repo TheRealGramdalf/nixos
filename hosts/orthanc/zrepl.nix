2dad18c9279467459d8dfc2e746d7a5f51cad993
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-13 19:02:52 +0000

    hosts/orthanc: init zrepl (disabled)

---
+++ hosts/orthanc/zrepl.nix
@@ -0,0 +1,22 @@
+_: {
+  services.zrepl = {
+    enable = true;
+    settings = {
+      jobs = [
+        {
+          name = "aer-offsite-backup";
+          type = "sink";
+          root_fs = "isengard/sink";
+          serve = {
+            ca = "/persist/secrets/zrepl/aer.crt";
+            cert = "/persist/secrets/zrepl/orthanc.crt";
+            key = "/persist/secrets/zrepl/orthanc.key";
+            server_cn = "aer";
+            listen = ":8888";
+            type = "tls";
+          };
+        }
+      ];
+    };
+  };
+}
