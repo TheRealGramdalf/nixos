f92b73be73a3793b7e1543caa89be7c334f13174
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-13 19:02:31 +0000

    hosts/aer: add zrepl (disabled)

---
+++ hosts/aer/services/zrepl.nix
@@ -0,0 +1,49 @@
+_: {
+  services.zrepl = {
+    enable = true;
+    settings = {
+      jobs = [
+        {
+          name = "offsite-backup";
+          connect = {
+            ca = "/persist/secrets/zrepl/orthanc.crt";
+            cert = "/persist/secrets/zrepl/aer.crt";
+            key = "/persist/secrets/zrepl/aer.key";
+            server_cn = "orthanc";
+            address = "orthanc.netbird.selfhosted:8888";
+            type = "tls";
+          };
+          filesystems = {
+            "tank/smb<" = true;
+            "tank/media<" = true;
+            "medusa/services<" = true;
+            "aer-zroot/safe/persist<" = true;
+          };
+          pruning = {
+            keep_receiver = [
+              {
+                grid = "1x1h(keep=all) | 24x1h | 30x1d | 6x30d";
+                # Only prune zrepl created snapshots
+                regex = "^zrepl_";
+                type = "grid";
+              }
+            ];
+            keep_sender = [
+              {type = "not_replicated";}
+              {
+                count = 10;
+                type = "last_n";
+              }
+            ];
+          };
+          snapshotting = {
+            interval = "10m";
+            prefix = "zrepl_";
+            type = "periodic";
+          };
+          type = "push";
+        }
+      ];
+    };
+  };
+}
