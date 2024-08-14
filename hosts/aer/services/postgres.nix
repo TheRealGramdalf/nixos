a9b83ad097f276e8f6bb9ff959373f8c1bc7367d
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-14 19:53:03 +0000

    hosts/aer: try to fix postgres authentication

--- hosts/aer/services/postgres.nix
+++ hosts/aer/services/postgres.nix
@@ -34,8 +34,14 @@ in {
     #  # Let other names login as themselves
     #  superuser_map      /^(.*)$   \1
     #'';
-    authentication = ''
+    authentication = lib.mkForce ''
+      # Trust the unix socket
       local all all trust
+
+      host  all all 127.0.0.1/32 scram-sha-256
+      host  all all ::1/128      scram-sha-256
+      host  all all 127.0.0.1/32 md5
+      host  all all ::1/128      md5
     '';
     initialScript = pkgs.writeText "init-sql-script" ''
       CREATE ROLE pgadmin WITH PASSWORD 'pgadmin' SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS LOGIN;
