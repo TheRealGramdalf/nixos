487d091f8b83c576affc8ce3ee3cbd260ff37ecb
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-17 01:00:35 +0000

    hosts/aer: reenable docker bridge (presumably for traefik)

--- hosts/aer/docker.nix
+++ hosts/aer/docker.nix
@@ -6,8 +6,6 @@
     package = pkgs.docker_26;
     storageDriver = "zfs";
     daemon.settings = {
-      # Disable the default docker0 bridge
-      bridge = "none";
       # ipv6 support isn't here yet
       ipv6 = false;
     };
