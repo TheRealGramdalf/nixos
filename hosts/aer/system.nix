32d53ccecd6beccaef41c97087b992b42ad58e58
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-30 20:25:12 +0000

    hosts/aer: Use patched `nh` package

--- hosts/aer/system.nix
+++ hosts/aer/system.nix
@@ -7,5 +7,17 @@ _: {
   programs.nh = {
     enable = true;
     flake = "/etc/nixos";
+    package = final: prev: {
+      nh = prev.nh.overrideAttrs (oldAttrs: {
+        patches =
+          oldAttrs.patches
+          ++ [
+            (prev.fetchpatch2 {
+              url = "https://github.com/PaulGrandperrin/nh-root/commit/bea0ce26e4b1a260e285164c49456d70d346c924.patch"; # don't bail when non-root
+              hash = "sha256-w8/nfMkk/CeOaLW2XIUvKs7//bGm11Cj6ifyTYzlqjo=";
+            })
+          ];
+      });
+    };
   };
 }
