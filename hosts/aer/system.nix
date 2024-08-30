985905b16943d6d600702acf8e88525b0aee3d28
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-30 20:14:19 +0000

    hosts/aer: enable `nh`

--- hosts/aer/system.nix
+++ hosts/aer/system.nix
@@ -3,4 +3,9 @@ _: {
   system.etc.overlay.enable = false;
   users.mutableUsers = false;
   users.users."root".hashedPasswordFile = "/persist/secrets/root.pw";
+
+  programs.nh = {
+    enable = true;
+    flake = "/etc/nixos";
+  };
 }
