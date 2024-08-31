1213ddb22d6f908b1b825e0ac0a893126859c9b3
TheRealGramdalf <gramdalftech@gmail.com>
2024-08-31 00:20:13 +0000

    hosts/aer: add nh patch

--- hosts/aer/system.nix
+++ hosts/aer/system.nix
@@ -1,4 +1,17 @@
-_: {
+{pkgs, ...}: {
+    nixpkgs.overlays = [
+    (final: prev: {
+      # Allow nh to run as root user
+      nh = prev.nh.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches ++ [
+          (prev.fetchpatch2 {
+            url = "https://github.com/PaulGrandperrin/nh-root/commit/bea0ce26e4b1a260e285164c49456d70d346c924.patch";
+            hash = "sha256-w8/nfMkk/CeOaLW2XIUvKs7//bGm11Cj6ifyTYzlqjo=";
+          })
+        ];
+      });
+    })
+  ];
   boot.initrd.systemd.enable = true;
   system.etc.overlay.enable = false;
   users.mutableUsers = false;
@@ -7,17 +20,6 @@ _: {
   programs.nh = {
     enable = true;
     flake = "/etc/nixos";
-    package = final: prev: {
-      nh = prev.nh.overrideAttrs (oldAttrs: {
-        patches =
-          oldAttrs.patches
-          ++ [
-            (prev.fetchpatch2 {
-              url = "https://github.com/PaulGrandperrin/nh-root/commit/bea0ce26e4b1a260e285164c49456d70d346c924.patch"; # don't bail when non-root
-              hash = "sha256-w8/nfMkk/CeOaLW2XIUvKs7//bGm11Cj6ifyTYzlqjo=";
-            })
-          ];
-      });
-    };
+    package = pkgs.nh
   };
 }
