4a332f53adcab2c09acbc547e7d79a3cb35ed1a3
TheRealGramdalf <gramdalftech@gmail.com>
2024-09-09 19:40:19 +0000

    hosts/muffin-time: enable SDDM wayland

--- hosts/muffin-time/kde.nix
+++ hosts/muffin-time/kde.nix
@@ -4,6 +4,8 @@
     enable = true;
     enableQt5Integration = true;
   };
+  # Enable Wayland in SDDM so the system doesn't need X11
+  services.displayManager.sddm.wayland.enable = true;
   
   # Firefox slightly more integrated (i.e. KDE Connect)
   programs.firefox.enable = true;
