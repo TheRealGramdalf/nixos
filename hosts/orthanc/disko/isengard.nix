7fc4256abeb7bf03a18f4f5c2807879bd0726ddc
TheRealGramdalf <gramdalftech@gmail.com>
2024-09-14 19:20:23 +0000

    hosts/orthanc: add zpool topology/disk IDs

--- hosts/orthanc/disko/isengard.nix
+++ hosts/orthanc/disko/isengard.nix
@@ -1,8 +1,47 @@
 # Isengard: Offsite backup pool matching hosts/aer/tank in capacity
-{
+let
+  fullzfs = {
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions."zfs" = {
+        size = "100%";
+        content = {
+          type = "zfs";
+          pool = "isengard";
+        };
+      };
+    };
+  };
+  vd1-1 = { device = "/dev/disk/by-id/wwn-0x5000c500626b52eb"; } // fullzfs;
+  vd1-2 = { device = "/dev/disk/by-id/wwn-0x5000c500638b7f17"; } // fullzfs;
+  vd1-3 = { device = "/dev/disk/by-id/wwn-0x5000c5008370435f"; } // fullzfs;
+  vd1-4 = { device = "/dev/disk/by-id/wwn-0x50014ee263d62711"; } // fullzfs;
+  vd1-5 = { device = "/dev/disk/by-id/wwn-0x50014ee263d636da"; } // fullzfs;
+  vd1-6 = { device = "/dev/disk/by-id/wwn-0x5000c50083702c0f"; } // fullzfs;
+in {
   disko.devices = {
+    disk = {
+      inherit vd1-1 vd1-2 vd1-3 vd1-4 vd1-5 vd1-6;
+    };
     zpool."isengard" = {
       type = "zpool";
+      mode.topology = {
+        type = "topology";
+        vdev = [
+          {
+            mode = "raidz2";
+            members = [
+              "vd1-1"
+              "vd1-2"
+              "vd1-3"
+              "vd1-4"
+              "vd1-5"
+              "vd1-6"
+            ];
+          }
+        ];
+      };
       options.ashift = "12";
       rootFsOptions = {
         # These are inherited to all child datasets as the default value
