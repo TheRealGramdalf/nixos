6c3bb8b507cf19f9e8cf4d45d6cbf22b038db6a8
TheRealGramdalf <gramdalftech@gmail.com>
2024-09-17 00:12:54 +0000

    hosts/aer: remove nh overlay

--- hosts/aer/system.nix
+++ hosts/aer/system.nix
@@ -1,19 +1,4 @@
 {pkgs, ...}: {
-  nixpkgs.overlays = [
-    (final: prev: {
-      # Allow nh to run as root user
-      nh = prev.nh.overrideAttrs (oldAttrs: {
-        patches =
-          oldAttrs.patches
-          ++ [
-            (prev.fetchpatch2 {
-              url = "https://github.com/PaulGrandperrin/nh-root/commit/bea0ce26e4b1a260e285164c49456d70d346c924.patch";
-              hash = "sha256-w8/nfMkk/CeOaLW2XIUvKs7//bGm11Cj6ifyTYzlqjo=";
-            })
-          ];
-      });
-    })
-  ];
   boot.initrd.systemd.enable = true;
   system.etc.overlay.enable = false;
   users.mutableUsers = false;
@@ -22,6 +7,5 @@
   programs.nh = {
     enable = true;
     flake = "/etc/nixos";
-    package = pkgs.nh;
   };
 }
