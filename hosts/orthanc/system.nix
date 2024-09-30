f07413ae6fe0f6f1f32c0553a82d1451d35ed525
TheRealGramdalf <gramdalftech@gmail.com>
2024-09-30 22:35:04 +0000

    hosts/orthanc: add tmux

--- hosts/orthanc/system.nix
+++ hosts/orthanc/system.nix
@@ -1,5 +1,8 @@
-_: {
+{pkgs, ...}: {
   boot.initrd.systemd.enable = true;
   users.mutableUsers = false;
   users.users."root".hashedPasswordFile = "/persist/secrets/root.pw";
+  environment.systemPackages = [
+    pkgs.tmux
+  ];
 }
