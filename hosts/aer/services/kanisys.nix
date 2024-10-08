eae6798d0c0d838e4bf77f6ba8540b2fa6685095
TheRealGramdalf <gramdalftech@gmail.com>
2024-10-08 01:21:26 +0000

    hosts/aer: add changes from stash for posterity

--- hosts/aer/services/kanisys.nix
+++ hosts/aer/services/kanisys.nix
@@ -1,9 +1,5 @@
-{
-  lib,
-  config,
-  ...
-}: let
-  inherit (lib) mapAttrs;
+{lib, config, ...}: let
+  inherit (lib) mapAttrs filterAttrs;
   #sysFiltered = filterAttrs (
   #    n: v:
   #      !isNull (builtins.match "([[:xdigit:]]{8}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{4}-[[:xdigit:]]{12})" v.serviceConfig.Group)
@@ -12,16 +8,14 @@
   #      #&& config.users.groups.${v.serviceConfig.Group} != {}
   #  )
   #  config.systemd.services;
-  sysMapped =
-    mapAttrs (
-      n: _v: {
-        services.n = {
-          after = ["kanidm-unixd.service"];
-          requires = ["kanidm-unixd.service"];
-        };
-      }
-    )
-    config.systemd.services;
+  sysMapped = mapAttrs (
+    n: v: {
+      services.n = {
+        after = ["kanidm-unixd.service"];
+        requires = ["kanidm-unixd.service"];
+      };
+    }
+  ) config.systemd.services;
 in {
   systemd.services = sysMapped;
 }
