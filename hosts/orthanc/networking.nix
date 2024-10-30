467b20ce6a0fbe452a9fae80d70abd46b9f38bb0
TheRealGramdalf <gramdalftech@gmail.com>
2024-10-30 01:47:04 +0000

    treewide: nix fmt

--- hosts/orthanc/networking.nix
+++ hosts/orthanc/networking.nix
@@ -28,15 +28,17 @@ _: {
           Domains = ["local"];
         };
         routes = [
-          {routeConfig = {
-            # ip route add
-            # default
-            Destination = "0.0.0.0/0";
-            Scope = "global";
-            # via *
-            Gateway = "_dhcp4";
-            # `dev *` is covered by the network match section 
-          };}
+          {
+            routeConfig = {
+              # ip route add
+              # default
+              Destination = "0.0.0.0/0";
+              Scope = "global";
+              # via *
+              Gateway = "_dhcp4";
+              # `dev *` is covered by the network match section
+            };
+          }
         ];
       };
     };
