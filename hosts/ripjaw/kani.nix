b961b6440446ab8974ebd36273670121a5aca48e
TheRealGramdalf <gramdalftech@gmail.com>
2024-12-01 00:13:17 +0000

    hosts/ripjaw: test v1 unixd settings

--- hosts/ripjaw/kani.nix
+++ hosts/ripjaw/kani.nix
@@ -1,24 +1,24 @@
 let
-  mainGroup = "50bc3d3a-87cd-4e90-ac6d-fb97f9b1a051"; # ripjaw-login
+  mainGroup = "ripjaw-login"; # ripjaw-login #50bc3d3a-87cd-4e90-ac6d-fb97f9b1a051
 in {
   services.kanidm = {
     clientSettings.uri = "https://auth.aer.dedyn.io";
     enablePam = true;
     unixSettings = {
-      version = "2";
+      #version = "2";
       pam_allowed_login_groups = ["${mainGroup}"];
-      kanidm = {
-        map_group = [
-          {
-            local = "video";
-            "with" = "${mainGroup}";
-          }
-          {
-            local = "networkmanager";
-            "with" = "${mainGroup}";
-          }
-        ];
-      };
+      #kanidm = {
+      #  map_group = [
+      #    {
+      #      local = "video";
+      #      "with" = "${mainGroup}";
+      #    }
+      #    {
+      #      local = "networkmanager";
+      #      "with" = "${mainGroup}";
+      #    }
+      #  ];
+      #};
     };
   };
 }
