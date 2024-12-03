be66c5392f4de1526c12b41bec85619c6f3f3cfd
TheRealGramdalf <gramdalftech@gmail.com>
2024-12-03 22:56:07 +0000

    hosts/ripjaw: add minegrub theme

--- hosts/ripjaw/hardware.nix
+++ hosts/ripjaw/hardware.nix
@@ -1,8 +1,31 @@
 {
   config,
   lib,
+  inputs,
   ...
-}: {
+}: let
+  inherit (config.system) nixos;
+  inherit (builtins) substring;
+  inherit (inputs.nixpkgs) lastModifiedDate;
+  dateFormatted = (
+    "(" +
+    # Year
+    "${substring 0 4 lastModifiedDate}" +
+    "/" +
+    # Month
+    "${substring 4 2 lastModifiedDate}" +
+    "/" +
+    # Day
+    "${substring 6 2 lastModifiedDate}" +
+    ", " +
+    # Hour
+    "${substring 8 2 lastModifiedDate}" +
+    ":" +
+    # Minute
+    "${substring 10 2 lastModifiedDate}" +
+    ")"
+  );
+in {
   boot.initrd.availableKernelModules = ["ahci" "ohci_pci" "ehci_pci" "pata_atiixp" "firewire_ohci" "usb_storage" "sd_mod" "sr_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
@@ -11,11 +34,25 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableAllFirmware = true;
 
+  imports = [ inputs.minegrub-theme.nixosModules.default];
   boot.loader.grub = {
     enable = true;
     # This device needs to be an entire disk, not a partition!
     device = "/dev/disk/by-id/wwn-0x50025385500dc182";
-    # Could probably use https://github.com/Lxtharia/minegrub-world-sel-theme
+    minegrub-world-sel = {
+      enable = true;
+      customIcons = [{
+        name = "nixos";
+        lineTop = "${nixos.distroName} '${nixos.codeName}' ${dateFormatted}";
+        lineBottom = "Survival Mode, No Cheats, Version: ${nixos.version}";
+        # Icon: you can use an icon from the remote repo, or load from a local file
+        imgName = "nixos";
+        # customImg = builtins.path {
+        #   path = ./nixos-logo.png;
+        #   name = "nixos-img";
+        # };
+      }];
+    };
   };
 
   fileSystems."/" = {
