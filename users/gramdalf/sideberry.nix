6e1b6f2ce92931d593ebfa173b83186c33a6a2b3
TheRealGramdalf <gramdalftech@gmail.com>
2024-12-12 19:01:29 +0000

    users/gramdalf: fix sideberry hide native tab bar

--- users/gramdalf/sideberry.nix
+++ users/gramdalf/sideberry.nix
@@ -21,20 +21,36 @@ _: {
       };
       userChrome = ''
         /* Hide the native tab bar when sideberry is active */
-
-        #main-window #titlebar {
-          overflow: hidden;
-          transition: height 0.3s 0.3s !important;
+        #main-window #TabsToolbar .toolbar-items {
+        overflow: unset;
+        transition: height 0.3s 0.3s !important;
         }
+        
         /* Default state: Set initial height to enable animation */
-        #main-window #titlebar { height: 3em !important; }
-        #main-window[uidensity="touch"] #titlebar { height: 3.35em !important; }
-        #main-window[uidensity="compact"] #titlebar { height: 2.7em !important; }
+        #main-window #TabsToolbar .toolbar-items {
+        height: 3em !important;
+        }
+        
+        #main-window[uidensity="touch"] #TabsToolbar .toolbar-items {
+        height: 3.35em !important;
+        }
+        #main-window[uidensity="compact"] #TabsToolbar .toolbar-items {
+        height: 2.7em !important;
+        }
+        
+        #main-window[uidensity="normal"] #TabsToolbar .toolbar-items {
+        height: 3.11em !important;
+        }
+        
         /* Hidden state: Hide native tabs strip */
-        #main-window[titlepreface*="sb: "] #titlebar { height: 0 !important; }
+        #main-window[titlepreface="​"] #TabsToolbar .toolbar-items {
+        height: 0px !important;
+        }
+        
         /* Hidden state: Fix z-index of active pinned tabs */
-        #main-window[titlepreface*="sb: "] #tabbrowser-tabs { z-index: 0 !important; }
-        /* Todo: make the close button (`toolbarbutton.titlebar-button.titlebar-close`) visible when native bar is disabled */
+        #main-window[titlepreface="​"] #tabbrowser-tabs {
+        z-index: 0 !important;
+        }
 
         /* Hide the sidebars' dropdown menu & splitter */
         #sidebar-splitter,
