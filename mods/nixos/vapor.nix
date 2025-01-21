9d23b0b64168ebb23c69882a3cd2e0937f90b997
TheRealGramdalf <gramdalftech@gmail.com>
2025-01-21 04:17:03 +0000

    mods/vapor: rename deprecated option name

--- mods/nixos/vapor.nix
+++ mods/nixos/vapor.nix
@@ -101,7 +101,7 @@ in {
     services.displayManager.sessionPackages = mkIf cfg.gamescopeSession.enable [gamescopeSessionFile];
 
     # optionally enable 32bit pulseaudio support if pulseaudio is enabled
-    hardware.pulseaudio.support32Bit = config.hardware.pulseaudio.enable;
+    services.pulseaudio.support32Bit = config.hardware.pulseaudio.enable;
 
     hardware.steam-hardware.enable = true;
 
