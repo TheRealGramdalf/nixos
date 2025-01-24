83703d53229bc5287994df9d5dce895a16a3948f
TheRealGramdalf <gramdalftech@gmail.com>
2025-01-24 19:01:41 +0000

    hosts/ripjaw: add jhon

--- flake.nix
+++ flake.nix
@@ -68,13 +68,19 @@
                 ./mods/home/main.nix
               ];
               users."games" = import ./users/games/main.nix;
+              users."jhon" = import ./users/jhon/main.nix;
             };
-            users.mutableUsers = false;
             users.users."games" = {
               isNormalUser = true;
               hashedPasswordFile = "/persist/secrets/passwdfile.games";
               extraGroups = ["video" "network"];
             };
+            users.users."jhon" = {
+              isNormalUser = true;
+              hashedPasswordFile = "/persist/secrets/passwdfile.jhon";
+              extraGroups = ["video" "network" "render"];
+            };
+            users.mutableUsers = false;
           }
         ];
       };
---
+++ users/jhon/home.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  home = {
+    username = "jhon";
+    homeDirectory = "/home/jhon";
+    stateVersion = "25.05";
+
+    packages = with pkgs; [
+      bitwarden
+      picard # MusicBrainz Tagger
+      vlc # VHS ripping and local media playback
+      obs-studio # Recording for anything in general
+      handbrake # For more manual ripping etc
+      finamp # Music only from jellyfin
+      jellyfin-media-player # Official client, for movies/tv shows
+      spotify
+    ];
+  };
+}
