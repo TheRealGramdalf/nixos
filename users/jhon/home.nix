b458cee8fa90dc398d1f0f19df49d7c9b3269d05
TheRealGramdalf <gramdalftech@gmail.com>
2025-02-23 01:11:07 +0000

    users/jhon: remove spotify collision

--- users/jhon/home.nix
+++ users/jhon/home.nix
@@ -25,7 +25,6 @@ in {
       handbrake # For more manual ripping etc
       finamp # Music only from jellyfin
       jellyfin-media-player # Official client, for movies/tv shows
-      spotify
     ];
   };
 }
