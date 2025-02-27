83949e093c8c4cb3d536bc4a547d86ed353df3be
TheRealGramdalf <gramdalftech@gmail.com>
2025-02-27 06:00:08 +0000

    users/jhon: remove podcasts

--- users/jhon/home.nix
+++ users/jhon/home.nix
@@ -8,7 +8,7 @@ in {
     enable = true;
     enabledExtensions = with spicePkgs.extensions; [
       adblock
-      randomBadToTheBoneRiff
+      hidePodcasts
     ];
     theme = spicePkgs.themes.comfy;
   };
