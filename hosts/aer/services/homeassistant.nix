3674e957231a7269704ac1228eb10c42e8a14170
TheRealGramdalf <gramdalftech@gmail.com>
2025-03-27 17:46:19 -0700

    hosts/aer: revert TLS changes

--- hosts/aer/services/homeassistant.nix
+++ hosts/aer/services/homeassistant.nix
@@ -79,7 +79,7 @@ in {
     "${mq}".settings = {
       tcp.routers."${mq}" = {
         rule = "HostSNI(`${mq}.aer.dedyn.io`)";
-        tls = false;
+        tls = true;
         service = "${mq}";
         middlewares = "local-only";
       };
