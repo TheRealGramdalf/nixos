4c2d9eda9f47c81cda9eadb4d14acd0d454d8f80
TheRealGramdalf <gramdalftech@gmail.com>
2025-03-31 18:06:49 -0700

    pkgs/tasmota: mostly working version

--- pkgs/tasmota.nix
+++ pkgs/tasmota.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   lib,
   stdenv,
   fetchFromGitHub,
@@ -11,13 +10,28 @@
   userConfig ? null,
   variant ? "tasmota",
   version ? "v14.5.0"
-}: let
-  repository = fetchFromGitHub {
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tasmota";
+  inherit version;
+  src = fetchFromGitHub {
     owner = "arendst";
     repo = "Tasmota";
-    rev = "v14.5.0";
+    rev = version;
     hash = "sha256-edJ+Zh0Sx/DF3v3zqXizE8x7uuWwINYg/Twch/E3GRQ=";
   };
+  preBuild = ''
+    echo "Writing settings override..."
+    echo ${userConfig} > tasmota/user_config_override.h
+  '';
+  buildPhase = ''
+    platformio run -e ${variant} --disable-auto-clean
+  '';
+  installPhase = ''
+    mkdir $out
+    cp -R build_output/* $out
+  '';
+
   nativeBuildInputs = [
     esptool
     #espflash
@@ -30,35 +44,7 @@
     python312Packages.zopfli
     python312Packages.wheel
   ];
-in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "tasmota";
-  inherit version;
-  src = pkgs.runCommand "${version}-pkg-install" {
-    inherit nativeBuildInputs;
-    outputHashAlgo = null;
-    outputHashMode = "recursive";
-    outputHash = "sha256-edJ+Zh0Sx/DF3v3zqXizE8x7uuWwINYg/Twch/E3GRQ=";
-  } ''
-    mkdir $out
-    cd $out
-    cp -r ${repository}/. .
-    platformio pkg install -e ${variant}
-  '';
-  preBuild = ''
-    echo "Writing settings override..."
-    echo ${userConfig} > tasmota/user_config_override.h
-  '';
-  buildPhase = ''
-    pio pkg list -e ${variant}
-    platformio run -e ${variant} --disable-auto-clean
-  '';
-  installPhase = ''
-    mkdir $out
-    cp -R build_output/* $out
-  '';
 
-  inherit nativeBuildInputs;
   meta = {
     description = "Tasmota firmware";
     homepage = "https://tasmota.github.io";
