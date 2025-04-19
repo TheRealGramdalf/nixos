ed8efe5127b7dadabbb208d0d8daa86d7f0c1b55
TheRealGramdalf <gramdalftech@gmail.com>
2025-04-18 19:47:31 -0700

    lib: add mkUnixdService

--- lib/main.nix
+++ lib/main.nix
@@ -1,7 +1,21 @@
 {inputs}: let
-  inherit (inputs.nixpkgs) lib;
+  inherit (inputs.nixpkgs) pkgsLib;
 in {
-  tome.lib = {
+  lib = {
     #mkTraefik = servicename: lbserver:
+    mkUnixdService = service: user: group: {
+      "${service}" = {
+        serviceConfig = pkgsLib.mkIf (user || group != null) {
+          User = user;
+          Group = group;
+        };
+        after = [
+          config.systemd.services."kanidm-unixd".name
+        ];
+        requires = [
+          config.systemd.services."kanidm-unixd".name
+        ];
+      };
+    };
   };
 }
