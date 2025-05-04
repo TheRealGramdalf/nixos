496316a048e2705afdfc939a7ff6312eb97a127b
TheRealGramdalf <gramdalftech@gmail.com>
2025-05-04 12:48:27 -0700

    lib: add extraServiceConfig

--- lib/main.nix
+++ lib/main.nix
@@ -1,17 +1,22 @@
-lib: {
+lib: let
+  inherit (lib) mkIf mkForce;
+in {
   #mkTraefik = servicename: lbserver:
   mkUnixdService = {
     nixosConfig,
     serviceName,
     serviceUser ? null,
     serviceGroup ? null,
+    extraServiceConfig ? null,
     ...
   }: {
     "${serviceName}" = {
-      serviceConfig = lib.mkIf (serviceUser != null || serviceGroup != null) {
-        User = lib.mkIf (serviceUser != null) (lib.mkForce serviceUser);
-        Group = lib.mkIf (serviceGroup != null) (lib.mkForce serviceGroup);
-      };
+      serviceConfig =
+        mkIf (serviceUser != null || serviceGroup != null) {
+          User = mkIf (serviceUser != null) (mkForce serviceUser);
+          Group = mkIf (serviceGroup != null) (mkForce serviceGroup);
+        }
+        // mkIf (extraServiceConfig != null) extraServiceConfig;
       # Using the `.name` attribute will throw an error if `kanidm-unixd` is not defined
       after = [
         nixosConfig.systemd.services."kanidm-unixd".name
