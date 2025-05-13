cb11bf62e6cf4d48f51d2b93f9783e24b58d072a
TheRealGramdalf <gramdalftech@gmail.com>
2025-05-12 21:54:42 -0700

    flake: add nixpkgs-stable, update lockfile

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "TheRealGramdalf's no-longer-experimental config";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
