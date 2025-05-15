117af198a9ba45d975d8fbdd1d0348d83821b2bd
TheRealGramdalf <gramdalftech@gmail.com>
2025-05-14 19:20:07 -0700

    hosts/aerwiar: use kanidm 1.6.X

--- hosts/aerwiar/configuration.nix
+++ hosts/aerwiar/configuration.nix
@@ -48,7 +48,7 @@
   services = {
     kanidm.enableClient = true;
     kanidm.clientSettings.uri = "https://auth.aer.dedyn.io";
-    kanidm.package = pkgs.kanidm_1_5;
+    kanidm.package = pkgs.kanidm_1_6;
     fwupd.enable = true;
   };
 
