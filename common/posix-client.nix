46d6ba618f95a77d6841012948b30b215ae7c598
TheRealGramdalf <gramdalftech@gmail.com>
2025-05-14 19:20:20 -0700

    common: use kanidm 1.6.X

--- common/posix-client.nix
+++ common/posix-client.nix
@@ -1,6 +1,6 @@
 {
   services.kanidm = {
-    package = pkgs.kanidm_1_4;
+    package = pkgs.kanidm_1_6;
     enablePam = true;
     clientSettings.uri = "https://auth.aer.dedyn.io";
     unixSettings = {
