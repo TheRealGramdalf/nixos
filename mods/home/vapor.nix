af9f0926833abd3d31a24811bd286bb08b9c2acd
TheRealGramdalf <gramdalftech@gmail.com>
2025-05-22 17:19:48 -0700

    mods/vapor: update module

--- mods/home/vapor.nix
+++ mods/home/vapor.nix
@@ -8,9 +8,17 @@
   inherit (lib) mkOption mkEnableOption literalExpression optionalAttrs optional optionals mkIf;
   inherit (lib) types;
   cfg = config.tomeutils.vapor;
+  oscfg = osConfig.tomeutils.vapor;
   gamescopeCfg = osConfig.programs.gamescope;
+  extraCompatPaths = lib.makeSearchPathOutput "steamcompattool" "" oscfg.extraCompatPackages;
 
-  extraCompatPaths = lib.makeSearchPathOutput "steamcompattool" "" cfg.extraCompatPackages;
+  steam-gamescope = let
+    exports = builtins.attrValues (builtins.mapAttrs (n: v: "export ${n}=${v}") oscfg.gamescopeSession.env);
+  in
+    pkgs.writeShellScriptBin "steam-gamescope" ''
+      ${builtins.concatStringsSep "\n" exports}
+      gamescope --steam ${toString oscfg.gamescopeSession.args} -- steam ${builtins.toString oscfg.gamescopeSession.steamArgs}
+    '';
   #steam-gamescope = let
   #  exports = builtins.attrValues (builtins.mapAttrs (n: v: "export ${n}=${v}") cfg.gamescopeSession.env);
   #in
@@ -30,116 +38,7 @@
 in {
   options.tomeutils.vapor = {
     enable = mkEnableOption "steam";
-
-    package = mkOption {
-      type = types.package;
-      default = pkgs.steam;
-      defaultText = literalExpression "pkgs.steam";
-      example = literalExpression ''
-        pkgs.steam-small.override {
-          extraEnv = {
-            MANGOHUD = true;
-            OBS_VKCAPTURE = true;
-            RADV_TEX_ANISO = 16;
-          };
-          extraLibraries = p: with p; [
-            atk
-          ];
-        }
-      '';
-      apply = steam:
-        steam.override (prev: {
-          extraEnv =
-            (optionalAttrs (cfg.extraCompatPackages != []) {
-              STEAM_EXTRA_COMPAT_TOOLS_PATHS = extraCompatPaths;
-            })
-            // (optionalAttrs cfg.extest.enable {
-              LD_PRELOAD = "${pkgs.pkgsi686Linux.extest}/lib/libextest.so";
-            })
-            // (prev.extraEnv or {});
-          extraLibraries = pkgs: let
-            prevLibs =
-              if prev ? extraLibraries
-              then prev.extraLibraries pkgs
-              else [];
-            additionalLibs = with osConfig.hardware.graphics;
-              if pkgs.stdenv.hostPlatform.is64bit
-              then [package] ++ extraPackages
-              else [package32] ++ extraPackages32;
-          in
-            prevLibs ++ additionalLibs;
-          extraPkgs = p: (cfg.extraPackages ++ optionals (prev ? extraPkgs) (prev.extraPkgs p));
-        });
-      #// optionalAttrs (cfg.gamescopeSession.enable && gamescopeCfg.capSysNice)
-      #{
-      #  buildFHSEnv = pkgs.buildFHSEnv.override {
-      #    # use the setuid wrapped bubblewrap
-      #    bubblewrap = "${config.security.wrapperDir}/..";
-      #  };
-      #}
-      description = ''
-        The Steam package to use. Additional libraries are added from the system
-        configuration to ensure graphics work properly.
-
-        Use this option to customise the Steam package rather than adding your
-        custom Steam to {option}`environment.systemPackages` yourself.
-      '';
-    };
-
-    extraPackages = mkOption {
-      type = types.listOf types.package;
-      default = [];
-      example = literalExpression ''
-        with pkgs; [
-          gamescope
-        ]
-      '';
-      description = ''
-        Additional packages to add to the Steam environment.
-      '';
-    };
-
-    extraCompatPackages = mkOption {
-      type = types.listOf types.package;
-      default = [];
-      example = literalExpression ''
-        with pkgs; [
-          proton-ge-bin
-        ]
-      '';
-      description = ''
-        Extra packages to be used as compatibility tools for Steam on Linux. Packages will be included
-        in the `STEAM_EXTRA_COMPAT_TOOLS_PATHS` environmental variable. For more information see
-        https://github.com/ValveSoftware/steam-for-linux/issues/6310.
-
-        These packages must be Steam compatibility tools that have a `steamcompattool` output.
-      '';
-    };
-
-    fontPackages = mkOption {
-      type = types.listOf types.package;
-      # `fonts.packages` is a list of paths now, filter out which are not packages
-      default = builtins.filter types.package.check osConfig.fonts.packages;
-      defaultText = literalExpression "builtins.filter types.package.check osConfig.fonts.packages";
-      example = literalExpression "with pkgs; [ source-han-sans ]";
-      description = ''
-        Font packages to use in Steam.
-
-        Defaults to system fonts, but could be overridden to use other fonts — useful for users who would like to customize CJK fonts used in Steam. According to the [upstream issue](https://github.com/ValveSoftware/steam-for-linux/issues/10422#issuecomment-1944396010), Steam only follows the per-user fontconfig configuration.
-      '';
-    };
-
-    extest.enable = mkEnableOption ''
-      Load the extest library into Steam, to translate X11 input events to
-      uinput events (e.g. for using Steam Input on Wayland)
-    '';
-
-    protontricks = {
-      enable = mkEnableOption "protontricks, a simple wrapper for running Winetricks commands for Proton-enabled games";
-      package = lib.mkPackageOption pkgs "protontricks" {};
-    };
   };
-
   config = mkIf cfg.enable {
     assertions = [
       {
@@ -147,23 +46,23 @@ in {
         message = "Vapor must be enabled system-wide for steam to work properly";
       }
     ];
-    tomeutils.vapor.extraPackages = cfg.fontPackages;
 
     home.packages =
       [
-        cfg.package
-        cfg.package.run
+        oscfg.package
+        oscfg.package.run
       ]
-      #++ optional cfg.gamescopeSession.enable steam-gamescope
-      ++ optional cfg.protontricks.enable (cfg.protontricks.package.override {inherit extraCompatPaths;});
-
-    #wayland.windowManager.hyprland.settings = lib.mkIf (config.wayland.windowManager.hyprland.enable) {
-    #  windowrulev2 = [
-    # Windowrules for steam (src: https://www.reddit.com/r/hyprland/comments/183tmfy/comment/kark334)
-    #windowrule=float,^(.*.exe)$
-    #windowrule=float,^(steam_app_.*)$
-    #windowrule=float,^(steam_proton)$
-    #  ];
-    #};
+      ++ lib.optional oscfg.gamescopeSession.enable steam-gamescope
+      ++ lib.optional oscfg.protontricks.enable (
+        oscfg.protontricks.package.override {inherit extraCompatPaths;}
+      );
   };
+  #wayland.windowManager.hyprland.settings = lib.mkIf (config.wayland.windowManager.hyprland.enable) {
+  #  windowrulev2 = [
+  # Windowrules for steam (src: https://www.reddit.com/r/hyprland/comments/183tmfy/comment/kark334)
+  #windowrule=float,^(.*.exe)$
+  #windowrule=float,^(steam_app_.*)$
+  #windowrule=float,^(steam_proton)$
+  #  ];
+  #};
 }
--- mods/nixos/vapor.nix
+++ mods/nixos/vapor.nix
@@ -9,12 +9,14 @@
   cfg = config.tomeutils.vapor;
   gamescopeCfg = config.programs.gamescope;
 
+  extraCompatPaths = lib.makeSearchPathOutput "steamcompattool" "" cfg.extraCompatPackages;
+
   steam-gamescope = let
     exports = builtins.attrValues (builtins.mapAttrs (n: v: "export ${n}=${v}") cfg.gamescopeSession.env);
   in
     pkgs.writeShellScriptBin "steam-gamescope" ''
       ${builtins.concatStringsSep "\n" exports}
-      gamescope --steam ${toString cfg.gamescopeSession.args} -- steam -tenfoot -pipewire-dmabuf
+      gamescope --steam ${toString cfg.gamescopeSession.args} -- steam ${builtins.toString cfg.gamescopeSession.steamArgs}
     '';
 
   gamescopeSessionFile =
@@ -29,6 +31,105 @@
 in {
   options.tomeutils.vapor = {
     enable = mkEnableOption "system-wide support for steam in userspace";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.steam;
+      defaultText = lib.literalExpression "pkgs.steam";
+      example = lib.literalExpression ''
+        pkgs.steam.override {
+          extraEnv = {
+            MANGOHUD = true;
+            OBS_VKCAPTURE = true;
+            RADV_TEX_ANISO = 16;
+          };
+          extraLibraries = p: with p; [
+            atk
+          ];
+        }
+      '';
+      apply = steam:
+        steam.override (
+          prev:
+            {
+              extraEnv =
+                (lib.optionalAttrs (cfg.extraCompatPackages != []) {
+                  STEAM_EXTRA_COMPAT_TOOLS_PATHS = extraCompatPaths;
+                })
+                // (lib.optionalAttrs cfg.extest.enable {
+                  LD_PRELOAD = "${pkgs.pkgsi686Linux.extest}/lib/libextest.so";
+                })
+                // (prev.extraEnv or {});
+              extraLibraries = pkgs: let
+                prevLibs =
+                  if prev ? extraLibraries
+                  then prev.extraLibraries pkgs
+                  else [];
+                additionalLibs = with config.hardware.graphics;
+                  if pkgs.stdenv.hostPlatform.is64bit
+                  then [package] ++ extraPackages
+                  else [package32] ++ extraPackages32;
+              in
+                prevLibs ++ additionalLibs;
+              extraPkgs = p: (cfg.extraPackages ++ lib.optionals (prev ? extraPkgs) (prev.extraPkgs p));
+            }
+            // lib.optionalAttrs (cfg.gamescopeSession.enable && gamescopeCfg.capSysNice) {
+              buildFHSEnv = pkgs.buildFHSEnv.override {
+                # use the setuid wrapped bubblewrap
+                bubblewrap = "${config.security.wrapperDir}/..";
+              };
+            }
+        );
+      description = ''
+        The Steam package to use. Additional libraries are added from the system
+        configuration to ensure graphics work properly.
+
+        Use this option to customise the Steam package rather than adding your
+        custom Steam to {option}`environment.systemPackages` yourself.
+      '';
+    };
+
+    extraPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+      example = lib.literalExpression ''
+        with pkgs; [
+          gamescope
+        ]
+      '';
+      description = ''
+        Additional packages to add to the Steam environment.
+      '';
+    };
+    extraCompatPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [];
+      example = lib.literalExpression ''
+        with pkgs; [
+          proton-ge-bin
+        ]
+      '';
+      description = ''
+        Extra packages to be used as compatibility tools for Steam on Linux. Packages will be included
+        in the `STEAM_EXTRA_COMPAT_TOOLS_PATHS` environmental variable. For more information see
+        https://github.com/ValveSoftware/steam-for-linux/issues/6310.
+
+        These packages must be Steam compatibility tools that have a `steamcompattool` output.
+      '';
+    };
+
+    fontPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      # `fonts.packages` is a list of paths now, filter out which are not packages
+      default = builtins.filter lib.types.package.check config.fonts.packages;
+      defaultText = lib.literalExpression "builtins.filter lib.types.package.check config.fonts.packages";
+      example = lib.literalExpression "with pkgs; [ source-han-sans ]";
+      description = ''
+        Font packages to use in Steam.
+
+        Defaults to system fonts, but could be overridden to use other fonts — useful for users who would like to customize CJK fonts used in Steam. According to the [upstream issue](https://github.com/ValveSoftware/steam-for-linux/issues/10422#issuecomment-1944396010), Steam only follows the per-user fontconfig configuration.
+      '';
+    };
 
     remotePlay.openFirewall = mkOption {
       type = types.bool;
@@ -72,11 +173,32 @@ in {
             type = types.attrsOf types.str;
             default = {};
             description = ''
-              Environmental variables to be passed to GameScope for the session.
+              Environment variables to be passed to GameScope for the session.
+            '';
+          };
+
+          steamArgs = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [
+              "-tenfoot"
+              "-pipewire-dmabuf"
+            ];
+            description = ''
+              Arguments to be passed to Steam for the session.
             '';
           };
         };
       };
+    };
+
+    extest.enable = lib.mkEnableOption ''
+      Load the extest library into Steam, to translate X11 input events to
+      uinput events (e.g. for using Steam Input on Wayland)
+    '';
+
+    protontricks = {
+      enable = lib.mkEnableOption "protontricks, a simple wrapper for running Winetricks commands for Proton-enabled games";
+      package = lib.mkPackageOption pkgs "protontricks" {};
     };
   };
 
@@ -97,11 +219,14 @@ in {
       };
     };
 
+    programs.steam.extraPackages = cfg.fontPackages;
+
     programs.gamescope.enable = mkDefault cfg.gamescopeSession.enable;
     services.displayManager.sessionPackages = mkIf cfg.gamescopeSession.enable [gamescopeSessionFile];
 
-    # optionally enable 32bit pulseaudio support if pulseaudio is enabled
-    services.pulseaudio.support32Bit = config.hardware.pulseaudio.enable;
+    # enable 32bit pulseaudio/pipewire support if needed
+    services.pulseaudio.support32Bit = config.services.pulseaudio.enable;
+    services.pipewire.alsa.support32Bit = config.services.pipewire.alsa.enable;
 
     hardware.steam-hardware.enable = true;
 
