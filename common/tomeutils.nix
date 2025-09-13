d78202620cd090d822a089c8b63835accde20c50
TheRealGramdalf <gramdalftech@gmail.com>
2025-09-12 20:39:53 -0700

    common/tomeutils: add unzip

--- common/tomeutils.nix
+++ common/tomeutils.nix
@@ -9,6 +9,7 @@
     tmux # Run detached shell sessions
     btop # System stats
     nix-tree # See where nix closure size goes
+    unzip # Good to have in general
   ];
   nix = {
     package = pkgs.nixVersions.nix_2_28;
