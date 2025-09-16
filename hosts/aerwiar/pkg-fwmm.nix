05eec5c6b71186d29b1050fbf3de26d35bbbee78
TheRealGramdalf <gramdalftech@gmail.com>
2025-09-16 13:17:43 -0700

    hosts/aerwiar: functional fwmm

--- hosts/aerwiar/pkg-fwmm.nix
+++ hosts/aerwiar/pkg-fwmm.nix
@@ -1,8 +1,9 @@
 {
   lib,
-  buildPythonApplication,
-  python3Packages,
-  fetchFromGithub,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  python,
   # deps
   pyserial,
   numpy,
@@ -11,30 +12,46 @@
   filedialpy ? null,
 }: let
   pname = "fwmm";
-  version = "1.0.2";
-in buildPythonApplication {
-  inherit pname version;
-  pyproject = false;
+  version = "v1.0.2";
 
-  src = fetchFromGithub {
+  pyWithLibs = python.buildEnv.override {
+    extraLibs = [
+      filedialpy
+      pyserial
+      numpy
+      aiohttp
+      python-crontab
+    ];
+    };
+in stdenv.mkDerivation {
+  inherit pname version;
+
+  src = fetchFromGitHub {
     owner = "DedFishy";
     repo = "FWMM";
-    inherit (rev) version;
-    sha256 = "sha256-kUcmeeHoCHMQuRrtOrLPrpvpW0LmRf2smbzncQIIC9Y=";
+    rev = version;
+    sha256 = "sha256-XgJ4MpUNS7lC2vFP57+mDq+FLZbm9vuw+2dbopAoHwM=";
   };
 
-  #build-system = with python3Packages; [ setuptools ];
-
-  pythonPath = [
-    filedialpy
-    pyserial
-    numpy
-    aiohttp
-    python-crontab
+  nativeBuildInputs = [
+    makeWrapper
   ];
 
+  installPhase = ''
+    mkdir $out
+    cp -R $src/* $out
+
+    substituteInPlace $out/main.py \
+      --replace-fail 'r+' 'r'
+
+    mkdir $out/bin
+    makeWrapper ${lib.getExe pyWithLibs} $out/bin/fwmm \
+      --add-flag $out/main.py \
+      --chdir $out
+  '';
+
   meta = {
-    changelog = "https://github.com/DedFishy/FWMM/releases/tag/v${version}";Python package for opening native file dialogs on linux, macOS and windows. It uses either zenity or kdialog on linux, pywin32 on windows and applescript on macOS
+    changelog = "https://github.com/DedFishy/FWMM/releases/tag/${version}";
     homepage = "https://github.com/DedFishy/FWMM";
     description = "A utility to customize what is displayed on your Framework 16 LED matrix module using a widget-based system.";
     license = lib.licenses.mit;
