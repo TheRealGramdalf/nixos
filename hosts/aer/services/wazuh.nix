a95bff05f7ca84973380f26d8df63bc218a7052b
TheRealGramdalf <gramdalftech@gmail.com>
2025-10-05 14:23:53 -0700

    hosts/aer: fix package reference

--- hosts/aer/services/wazuh.nix
+++ hosts/aer/services/wazuh.nix
@@ -1,6 +1,6 @@
 {inputs, lib, pkgs, ...}: {
   services.wazuh.agent = {
     enable = true;
-    package = inputs.outputs.packages.x86_64-linux.wazuh-agent;
+    package = inputs.self.packages.x86_64-linux.wazuh-agent;
   };
 }
