c0ce86664e61918bd7953501cf71678727989a5c
TheRealGramdalf <gramdalftech@gmail.com>
2025-10-12 15:14:52 -0700

    hosts/atreus: apply dbusmenu bandaid

--- hosts/atreus/peripherals.nix
+++ hosts/atreus/peripherals.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   hardware = {
-    ckb-next.enable = true;
+    ckb-next = {
+      enable = true;
+      package = pkgs.ckb-next.overrideAttrs (old: {
+        cmakeFlags = (old.cmakeFlags or []) ++ ["-DUSE_DBUS_MENU=0"];
+      });
+    };
     opentabletdriver.enable = true;
     openrazer = {
       enable = true;
