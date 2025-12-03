76e30a7691204f0b8fb569e31e19c527527422dd
TheRealGramdalf <gramdalftech@gmail.com>
2025-12-03 12:58:09 -0800

    flake: update stable pkgs

--- flake.nix
+++ flake.nix
@@ -7,9 +7,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
