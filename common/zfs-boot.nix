7a7fd43f0f9088d1204c760b1fc455988b75d8b6
TheRealGramdalf <gramdalftech@gmail.com>
2026-01-01 16:57:15 -0800

    treewide: use kernel 6_18

--- common/zfs-boot.nix
+++ common/zfs-boot.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages_6_17;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
   #boot.zfs = {
   #enabled = true; # This is enabled automatically
   #devNodes = /dev/disk/by-partlabel; # Might have issues with legacy BIOS, since it can't read GPT labels?
