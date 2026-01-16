c374175d326976ce299643a4b10288785ff44ab8
TheRealGramdalf <gramdalftech@gmail.com>
2026-01-15 16:42:13 -0800

    hosts/aer: enable CPU governor

--- hosts/aer/system.nix
+++ hosts/aer/system.nix
@@ -4,6 +4,8 @@ _: {
   users.mutableUsers = false;
   users.users."root".hashedPasswordFile = "/persist/root.pw";
 
+  services.auto-cpufreq.enable = true;
+
   programs.nh = {
     enable = true;
     flake = "/etc/nixos";
