496e72c2c0483910a97b7d817ccd2bc5cf6e99a8
TheRealGramdalf <gramdalftech@gmail.com>
2026-01-21 18:17:53 -0800

    hosts/klippy: add klipper firmware

--- hosts/klippy/klipper.nix
+++ hosts/klippy/klipper.nix
@@ -4,6 +4,11 @@ _: {
     mutableConfig = true;
     configDir = "/persist/services/klipper";
     configFile = "/persist/services/klipper/printer.cfg";
+    firmwares."anycubic-i3-mega" = {
+      serial = "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0";
+      enableKlipperFlash = true;
+      configFile = "/persist/services/klipper/firmware-config";
+    };
   };
   services.mainsail = {
     enable = true;
