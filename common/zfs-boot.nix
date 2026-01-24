a9624b344f410dc44b7d2be8a6281420b2301204
TheRealGramdalf <gramdalftech@gmail.com>
2026-01-23 18:01:12 -0800

    treewide: reenable zramswap

--- common/zfs-boot.nix
+++ common/zfs-boot.nix
@@ -5,8 +5,7 @@
   #enabled = true; # This is enabled automatically
   #devNodes = /dev/disk/by-partlabel; # Might have issues with legacy BIOS, since it can't read GPT labels?
   #};
-  # Zram might be causing double compression and causing the system to hang in an OOM state
   zramSwap = {
-    enable = false;
+    enable = true;
   };
 }
