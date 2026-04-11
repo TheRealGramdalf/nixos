b56f54e303f6b101af2813897c99d53876d38e11
TheRealGramdalf <gramdalftech@gmail.com>
2026-04-11 10:14:57 -0700

    hosts/aer: migrate kanidm syntax

--- hosts/aer/services/kanidm.nix
+++ hosts/aer/services/kanidm.nix
@@ -17,25 +17,29 @@ in {
   };
   services.kanidm = {
     package = pkgs.kanidm_1_8;
-    enableServer = true;
-    enablePam = true;
-    serverSettings = {
-      version = "2";
-      #log_level = "debug";
-      origin = "https://auth.aer.dedyn.io";
-      domain = "auth.aer.dedyn.io";
-      # Needs https://github.com/NixOS/nixpkgs/pull/323676 to work properly
-      #db_path = "${dataDir}/db/kanidm.db";
-      db_fs_type = "zfs";
-      tls_chain = "${dataDir}/chain.pem";
-      tls_key = "${dataDir}/key.pem";
-      bindaddress = "127.0.0.1:8443";
+    server = {
+      enable = true;
+      settings = {
+        version = "2";
+        #log_level = "debug";
+        origin = "https://auth.aer.dedyn.io";
+        domain = "auth.aer.dedyn.io";
+        # Needs https://github.com/NixOS/nixpkgs/pull/323676 to work properly
+        #db_path = "${dataDir}/db/kanidm.db";
+        db_fs_type = "zfs";
+        tls_chain = "${dataDir}/chain.pem";
+        tls_key = "${dataDir}/key.pem";
+        bindaddress = "127.0.0.1:8443";
+      };
     };
-    clientSettings.uri = cfg.serverSettings.origin;
-    unixSettings = {
-      kanidm.pam_allowed_login_groups = ["3fd496d1-bdcb-42b3-bbab-040b6befcaac"];
-      home_attr = "uuid";
-      home_alias = "spn";
+    client.settings.uri = cfg.serverSettings.origin;
+    unix = {
+      enable = true;
+      settings = {
+        kanidm.pam_allowed_login_groups = ["3fd496d1-bdcb-42b3-bbab-040b6befcaac"];
+        home_attr = "uuid";
+        home_alias = "spn";
+      };
     };
   };
 
