2377f4d1eecd21915d0ee6e08e3c3ce9cbd529aa
TheRealGramdalf <gramdalftech@gmail.com>
2026-06-18 12:07:26 -0700

    users/gramdalf: remove wireshark, add ldap build workaround, add wl-color-picker

--- users/gramdalf/home.nix
+++ users/gramdalf/home.nix
@@ -71,7 +71,6 @@
       btop
       inkscape
       picard # MusicBrainz Tagger
-      wireshark
       dive
       filezilla
       motrix # Download manager
@@ -115,7 +114,22 @@
       epiphany
       qbittorrent
       # Hypervisors
-      bottles
+      (pkgs.bottles.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv = args:
+          pkgs.buildFHSEnv (args
+            // {
+              multiPkgs = envPkgs: let
+                # Fetch original package list
+                originalPkgs = args.multiPkgs envPkgs;
+
+                # Disable tests for openldap
+                customLdap = envPkgs.openldap.overrideAttrs (_: {doCheck = false;});
+              in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [customLdap];
+            });
+      })
       # Games
       prismlauncher
       mangohud # System usage stats
@@ -136,6 +150,7 @@
       fastfetch
       tealdeer
       pdfmixtool
+      wl-color-picker
       #(ventoy.override {
       #  withGtk3 = true;
       #  defaultGuiType = "gtk3";
@@ -162,7 +177,22 @@
       cadaver
       piper
       freecad
-      lutris
+      (pkgs.lutris.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv = args:
+          pkgs.buildFHSEnv (args
+            // {
+              multiPkgs = envPkgs: let
+                # Fetch original package list
+                originalPkgs = args.multiPkgs envPkgs;
+
+                # Disable tests for openldap
+                customLdap = envPkgs.openldap.overrideAttrs (_: {doCheck = false;});
+              in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [customLdap];
+            });
+      })
     ];
   };
   programs = {
