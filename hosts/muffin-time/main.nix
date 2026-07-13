5a40e5321327301fcb592dd71b575004ff09440e
TheRealGramdalf <gramdalftech@gmail.com>
2026-07-13 10:50:46 -0700

    hosts/muffin-time: update stateversion (reinstall)

--- hosts/muffin-time/main.nix
+++ hosts/muffin-time/main.nix
@@ -10,7 +10,7 @@
   time.timeZone = "America/Vancouver";
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "24.11";
+  system.stateVersion = "26.05";
   networking = {
     hostName = "muffin-time";
     hostId = "04ff6c6c";
