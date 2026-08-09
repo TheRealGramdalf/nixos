b46fbb719dc4d423098bad1bdccf0326e6c19ec2
TheRealGramdalf <gramdalftech@gmail.com>
2026-08-09 14:10:20 -0700

    hosts/atreus: rename kanidm options

--- hosts/atreus/kanidm.nix
+++ hosts/atreus/kanidm.nix
@@ -2,32 +2,34 @@
   mainGroup = "0359756e-7db7-410a-bc16-89621465f64a"; # atreus-login
 in {
   services.kanidm = {
-    package = pkgs.kanidm_1_10;
-    clientSettings.uri = "https://auth.aer.dedyn.io";
-    enablePam = true;
-    unixSettings = {
-      version = "2";
-      kanidm = {
-        # In version 2, this is under `kanidm`
-        pam_allowed_login_groups = ["${mainGroup}"];
-        map_group = [
-          {
-            local = "video";
-            "with" = "${mainGroup}";
-          }
-          {
-            local = "networkmanager";
-            "with" = "${mainGroup}";
-          }
-          {
-            local = "timekpr";
-            "with" = "fd5a9395-68f4-42d6-a7f2-c64fd1f13e96";
-          }
-          {
-            local = "wheel";
-            "with" = "fd5a9395-68f4-42d6-a7f2-c64fd1f13e96";
-          }
-        ];
+    package = pkgs.kanidm_1_11;
+    client.settings.uri = "https://auth.aer.dedyn.io";
+    unix = {
+      enable = true;
+      settings = {
+        version = "2";
+        kanidm = {
+          # In version 2, this is under `kanidm`
+          pam_allowed_login_groups = ["${mainGroup}"];
+          map_group = [
+            {
+              local = "video";
+              "with" = "${mainGroup}";
+            }
+            {
+              local = "networkmanager";
+              "with" = "${mainGroup}";
+            }
+            {
+              local = "timekpr";
+              "with" = "fd5a9395-68f4-42d6-a7f2-c64fd1f13e96";
+            }
+            {
+              local = "wheel";
+              "with" = "fd5a9395-68f4-42d6-a7f2-c64fd1f13e96";
+            }
+          ];
+        };
       };
     };
   };
