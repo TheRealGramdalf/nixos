a2b5a78ff65215ef3557f90e93ce45cfbdfb7a6d
TheRealGramdalf <gramdalftech@gmail.com>
2026-09-23 22:36:29 -0700

    hosts/atreus: refactor users

--- hosts/atreus/users.nix
+++ hosts/atreus/users.nix
@@ -7,9 +7,8 @@
       ../../mods/home/main.nix
     ];
     users."meebling" = import ../../users/meebling/main.nix;
+    users."mlem" = import ../../users/meebling/main.nix;
     users."meeblingthedevilish" = import ../../users/meebling/devilish.nix;
-    users."zoom" = import ../../users/meebling/zoom.nix;
-    users."music" = import ../../users/meebling/music.nix;
   };
   users.mutableUsers = false;
   users.users."meebling" = {
@@ -17,19 +16,15 @@
     hashedPasswordFile = "/persist/secrets/passwdfile.meebling";
     extraGroups = ["video" "networkmanager"];
   };
+  users.users."mlem" = {
+    description = "School Only";
+    isNormalUser = true;
+    hashedPasswordFile = "/persist/secrets/passwdfile.meebling";
+    extraGroups = ["video" "networkmanager"];
+  };
   users.users."meeblingthedevilish" = {
     isNormalUser = true;
     hashedPasswordFile = "/persist/secrets/passwdfile.meeblingthedevilish";
-    extraGroups = ["video" "networkmanager"];
-  };
-  users.users."zoom" = {
-    isNormalUser = true;
-    hashedPasswordFile = "/persist/secrets/passwdfile.meebling";
-    extraGroups = ["video" "networkmanager"];
-  };
-  users.users."music" = {
-    isNormalUser = true;
-    hashedPasswordFile = "/persist/secrets/passwdfile.meebling";
     extraGroups = ["video" "networkmanager"];
   };
 }
